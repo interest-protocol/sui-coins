25efe7edb4da987aecdfe4aeb0f394cc6d252019
MarioBatalha <mariobatalha9@gmail.com>
2023-11-04 21:27:02 +0100

    feat(:sparkles:): add new fonts style

--- styles/index.ts
+++ styles/index.ts
@@ -5,7 +5,7 @@ export const GlobalStyles = css`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Roboto Mono', 'Share Tech Mono', monospace;
+    font-family: 'Satoshi-Medium', 'Proto Mono', monospace;
   }
 
   html {
