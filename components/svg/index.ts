c7f86aecdc176f299dbd0da0ab0f520cb984b86b
KipandaJr <antoniokipandacardoso@gmail.com>
2023-11-06 00:54:46 +0100

    feat: :sparkles: adding text field element

--- components/svg/index.ts
+++ components/svg/index.ts
@@ -1,5 +1,7 @@
 export { default as ComputerEyesSVG } from './computer-eyes';
 export { default as DiedComputerSVG } from './died-computer';
+export { default as ErrorSVG } from './error';
 export { default as HeartSVG } from './heart';
 export { default as IPXSVG } from './ipx';
 export { default as LogoSVG } from './logo';
+export { default as TickSVG } from './tick';
