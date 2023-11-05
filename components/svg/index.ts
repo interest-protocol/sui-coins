f0c002ea06b6a0a24402dc50c6c8abd01d73e6a7
git-marcopitra <marcomorete73@hotmail.com>
2023-11-05 21:34:30 +0100

    build: fix build

--- components/svg/index.ts
+++ components/svg/index.ts
@@ -1,3 +1,5 @@
+export { default as ComputerEyesSVG } from './computer-eyes';
+export { default as DiedComputerSVG } from './died-computer';
 export { default as HeartSVG } from './heart';
 export { default as IPXSVG } from './ipx';
 export { default as LogoSVG } from './logo';
