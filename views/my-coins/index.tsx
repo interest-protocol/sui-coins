fb889a7b72fe822c74e2cc043e5c5c38a11b911f
Okusindja de Almeida <okusindja@gmail.com>
2023-11-07 21:09:44 +0100

    ✨ feat: add error state UI on my coins

--- views/my-coins/index.tsx
+++ views/my-coins/index.tsx
@@ -4,13 +4,14 @@ import Skeleton from 'react-loading-skeleton';
 import { v4 } from 'uuid';
 
 import Layout from '@/components/layout';
+import { XCirlceSVG } from '@/svg';
 
 import { useGetAllCoinsWithMetadata } from './my-coins.hooks';
 import MyCoinsHeader from './my-coins-header';
 import MyCoinsItem from './my-coins-item';
 
 const MyCoins: FC = () => {
-  const { coins, isLoading } = useGetAllCoinsWithMetadata();
+  const { coins, isLoading, error } = useGetAllCoinsWithMetadata();
 
   return (
     <Layout>
@@ -22,35 +23,50 @@ const MyCoins: FC = () => {
       >
         My Coins
       </Box>
-      <Box
-        my="2xl"
-        borderRadius="m"
-        mx={[0, '11xl']}
-        bg="lowestContainer"
-        width={['calc(100vw - 3rem)', 'auto']}
-        boxShadow="0px 24px 46px -10px rgba(13, 16, 23, 0.16)"
-      >
-        <MyCoinsHeader />
-        {isLoading ? (
-          <Box
-            py="m"
-            px="l"
-            rowGap="m"
-            columnGap="xl"
-            display="grid"
-            cursor="pointer"
-            alignItems="center"
-            gridTemplateColumns="2rem 1fr 1fr 1fr 2rem"
-          >
-            <Box />
-            <Skeleton />
-            <Skeleton />
-            <Skeleton />
-          </Box>
-        ) : (
-          coins.map((coin) => <MyCoinsItem key={v4()} {...coin} />)
-        )}
-      </Box>
+      {error ? (
+        <Box
+          mt="3xl"
+          rowGap="xl"
+          color="error"
+          display="flex"
+          alignItems="center"
+          flexDirection="column"
+          justifyContent="center"
+        >
+          <XCirlceSVG maxWidth="4rem" maxHeight="4rem" width="100%" />
+          <Box>Oops! Something went wrong.</Box>
+        </Box>
+      ) : (
+        <Box
+          my="2xl"
+          borderRadius="m"
+          mx={[0, '11xl']}
+          bg="lowestContainer"
+          width={['calc(100vw - 3rem)', 'auto']}
+          boxShadow="0px 24px 46px -10px rgba(13, 16, 23, 0.16)"
+        >
+          <MyCoinsHeader />
+          {isLoading ? (
+            <Box
+              py="m"
+              px="l"
+              rowGap="m"
+              columnGap="xl"
+              display="grid"
+              cursor="pointer"
+              alignItems="center"
+              gridTemplateColumns="2rem 1fr 1fr 1fr 2rem"
+            >
+              <Box />
+              <Skeleton />
+              <Skeleton />
+              <Skeleton />
+            </Box>
+          ) : (
+            coins.map((coin) => <MyCoinsItem key={v4()} {...coin} />)
+          )}
+        </Box>
+      )}
     </Layout>
   );
 };
