1e1d853702dc07336af88cd29ea165e15bbdfd32
git-marcopitra <marcomorete73@hotmail.com>
2023-11-07 21:50:31 +0100

    fix: add error message and make it work

--- views/my-coins/index.tsx
+++ views/my-coins/index.tsx
@@ -1,9 +1,11 @@
 import { Box } from '@interest-protocol/ui-kit';
+import { useWalletKit } from '@mysten/wallet-kit';
 import { FC } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import { v4 } from 'uuid';
 
 import Layout from '@/components/layout';
+import WalletConnect from '@/components/wallet';
 import { XCirlceSVG } from '@/svg';
 
 import { useGetAllCoinsWithMetadata } from './my-coins.hooks';
@@ -11,7 +13,10 @@ import MyCoinsHeader from './my-coins-header';
 import MyCoinsItem from './my-coins-item';
 
 const MyCoins: FC = () => {
+  const { currentAccount } = useWalletKit();
   const { coins, isLoading, error } = useGetAllCoinsWithMetadata();
+
+  const isError = !currentAccount || error;
 
   return (
     <Layout>
@@ -23,7 +28,7 @@ const MyCoins: FC = () => {
       >
         My Coins
       </Box>
-      {error ? (
+      {isError ? (
         <Box
           mt="3xl"
           rowGap="xl"
@@ -34,13 +39,19 @@ const MyCoins: FC = () => {
           justifyContent="center"
         >
           <XCirlceSVG maxWidth="4rem" maxHeight="4rem" width="100%" />
-          <Box>Oops! Something went wrong.</Box>
+          <Box>
+            {!currentAccount
+              ? 'You are disconnect, please connect to your wallet.'
+              : 'Oops! Something went wrong.'}
+          </Box>
+          {!currentAccount && <WalletConnect />}
         </Box>
       ) : (
         <Box
           my="2xl"
           borderRadius="m"
           mx={[0, '11xl']}
+          overflow="hidden"
           bg="lowestContainer"
           width={['calc(100vw - 3rem)', 'auto']}
           boxShadow="0px 24px 46px -10px rgba(13, 16, 23, 0.16)"
@@ -48,16 +59,21 @@ const MyCoins: FC = () => {
           <MyCoinsHeader />
           {isLoading ? (
             <Box
-              py="m"
-              px="l"
               rowGap="m"
               columnGap="xl"
               display="grid"
+              py={['s', 'm']}
+              px={['s', 'l']}
               cursor="pointer"
               alignItems="center"
-              gridTemplateColumns="2rem 1fr 1fr 1fr 2rem"
+              borderTop="1px solid"
+              borderColor="outlineVariant"
+              gridTemplateColumns={[
+                '1fr 1fr 1fr 2rem',
+                '2rem 1fr 1fr 1fr 2rem',
+              ]}
             >
-              <Box />
+              <Box display={['none', 'block']} />
               <Skeleton />
               <Skeleton />
               <Skeleton />
