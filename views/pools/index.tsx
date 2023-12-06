b2e092c7943a517afcd325b2ac412b2747e29557
Okusindja de Almeida <91469357+okusindja@users.noreply.github.com>
2023-12-06 20:49:50 +0100

    ✨ add pool title bar (#31)
    * ✨ feat: add pool title bar
    
    * :lipstick: fix: review on ui
    
    * :lipstick: fix: review on ui
    
    ---------
    
    Co-authored-by: KipandaJr <antoniokipandacardoso@gmail.com>
    Co-authored-by: Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>

--- views/pools/index.tsx
+++ views/pools/index.tsx
@@ -1,14 +1,21 @@
-import { Typography } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
 
 import Layout from '@/components/layout';
+import { BNBSVG } from '@/svg';
+
+import PoolTitleBar from './pool-title-bar';
 
 const Pools: FC = () => {
+  const onBack = () => {
+    console.log('>>>>>>On Back');
+  };
   return (
     <Layout>
-      <Typography my="2xl" size="large" variant="display" textAlign="center">
-        Pools
-      </Typography>
+      <PoolTitleBar
+        onBack={onBack}
+        name="Pool Name"
+        iconTokenList={[BNBSVG, BNBSVG]}
+      />
     </Layout>
   );
 };
