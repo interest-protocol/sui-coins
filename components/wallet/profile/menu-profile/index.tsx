caae7cb9d031e0d0eeaf1984339c53e24fe017ee
git-marcopitra <marcomorete73@hotmail.com>
2024-01-31 01:18:07 +0100

    :bug: fix: wallet menu

--- components/wallet/profile/menu-profile/index.tsx
+++ components/wallet/profile/menu-profile/index.tsx
@@ -19,9 +19,12 @@ const MenuProfile: FC<MenuProfileProps> = ({
   handleOpenSwitch,
   handleCloseProfile,
 }) => {
-  const { network } = useNetwork();
-  const { disconnect, currentAccount } = useWalletKit();
   const { push } = useRouter();
+  const { network } = useNetwork();
+  const { breakpoints } = useTheme() as Theme;
+  const [isDesktop, setIsDesktop] = useState(false);
+  const { disconnect, currentAccount } = useWalletKit();
+
   const account = currentAccount?.address || '';
 
   const handleAction: Record<string, () => void | Promise<void>> = {
@@ -38,8 +41,6 @@ const MenuProfile: FC<MenuProfileProps> = ({
     },
   };
 
-  const [isDesktop, setIsDesktop] = useState(false);
-  const { breakpoints } = useTheme() as Theme;
   const handleSetDesktopView = () =>
     setIsDesktop(window.matchMedia(`(min-width: ${breakpoints[2]})`).matches);
 
@@ -48,30 +49,30 @@ const MenuProfile: FC<MenuProfileProps> = ({
   return (
     <Motion
       right="0"
-      top={['0', '0', '0', '3rem']}
-      overflow="auto"
       zIndex={1}
-      initial="closed"
-      borderRadius="unset"
-      position={['fixed', 'fixed', 'fixed', 'absolute']}
       bg="container"
+      display="flex"
+      borderRadius="s"
+      overflow="hidden"
+      flexDirection="column"
       variants={wrapperVariants}
-      animate={isOpen ? 'open' : 'closed'}
-      pointerEvents={isOpen ? 'auto' : 'none'}
       textTransform="capitalize"
+      top={['0', '0', '0', '3rem']}
+      justifyContent="space-between"
+      p={['xl', 'xl', 'xl', 'unset']}
+      initial={isOpen ? 'closed' : 'open'}
+      animate={isOpen ? 'open' : 'closed'}
+      pb={['7rem', '7rem', '7rem', 'unset']}
+      pointerEvents={isOpen ? 'auto' : 'none'}
       width={['100vw', '100vw', '100vw', '14.5rem']}
       height={['100vh', '100vh', '100vh', 'unset']}
-      p={['xl', 'xl', 'xl', 'unset']}
-      pb={['7rem', '7rem', '7rem', 'unset']}
-      display="flex"
-      flexDirection="column"
-      justifyContent="space-between"
+      position={['fixed', 'fixed', 'fixed', 'absolute']}
     >
       <Box display="flex" flexDirection="column" justifyContent="space-between">
         <Box
-          display={['flex', 'flex', 'flex', 'none']}
-          flexDirection="row-reverse"
           pb="l"
+          flexDirection="row-reverse"
+          display={['flex', 'flex', 'flex', 'none']}
         >
           <MenuButton handleClose={handleCloseProfile} />
         </Box>
