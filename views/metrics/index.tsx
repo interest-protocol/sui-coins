ae6461d337572278a760ada74585f05109e24ba9
MarioBatalha <mariobatalha9@gmail.com>
2024-02-09 00:08:53 +0100

    :bug: fix: conflicts

--- views/metrics/index.tsx
+++ views/metrics/index.tsx
@@ -4,20 +4,23 @@ import { FC } from 'react';
 import Layout from '@/components/layout';
 
 import InfoCardsList from './info-card';
-
+import TopCoinsTable from './top-tables/top-coins-table';
+import TopPoolsTable from './top-tables/top-pools-table';
 const Metrics: FC = () => (
   <Layout>
-    <Box maxWidth="100%">
-      <Typography
-        my="6xl"
-        size="small"
-        variant="display"
-        textAlign="center"
-        fontSize={['5xl', '8xl']}
-      >
-        Metrics
-      </Typography>
+    <Typography
+      my="6xl"
+      size="small"
+      variant="display"
+      textAlign="center"
+      fontSize={['5xl', '8xl']}
+    >
+      Metrics
+    </Typography>
+    <Box variant="container">
       <InfoCardsList />
+      <TopPoolsTable />
+      <TopCoinsTable />
     </Box>
   </Layout>
 );
