8000d968fbe698e3feaf0cec29ef1ddaa4eea934
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2024-02-09 10:40:12 +0100

    🧑‍💻  Remove the loader after load (#107)

--- views/components/select-nft-modal/nft-modal-item.tsx
+++ views/components/select-nft-modal/nft-modal-item.tsx
@@ -1,5 +1,5 @@
 import { Box, ProgressIndicator, Typography } from '@interest-protocol/ui-kit';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import { NFTModalItemProps } from './select-nft-modal.types';
 
@@ -10,60 +10,71 @@ const NFTModalItem: FC<NFTModalItemProps> = ({
   onClick,
   selected,
   updatedAt,
-}) => (
-  <Box
-    p="xl"
-    display="flex"
-    cursor="pointer"
-    color="onSurface"
-    alignItems="center"
-    justifyContent="space-between"
-    onClick={selected ? undefined : onClick}
-    transition="background 500ms ease-in-out"
-    nHover={{ bg: 'rgba(0, 83, 219, 0.08)' }}
-    bg={selected ? 'rgba(0, 83, 219, 0.08)' : 'unset'}
-  >
-    <Box display="flex" alignItems="center">
-      <Box
-        display="flex"
-        width="2.5rem"
-        height="2.5rem"
-        overflow="hidden"
-        borderRadius="xs"
-        alignItems="center"
-        position="relative"
-        justifyContent="center"
-      >
-        <Box position="absolute" top="-0.5rem">
-          <ProgressIndicator size={16} variant="loading" />
+}) => {
+  const [isImageLoading, setImageLoading] = useState(true);
+
+  return (
+    <Box
+      p="xl"
+      display="flex"
+      cursor="pointer"
+      color="onSurface"
+      alignItems="center"
+      justifyContent="space-between"
+      onClick={selected ? undefined : onClick}
+      transition="background 500ms ease-in-out"
+      nHover={{ bg: 'rgba(0, 83, 219, 0.08)' }}
+      bg={selected ? 'rgba(0, 83, 219, 0.08)' : 'unset'}
+    >
+      <Box display="flex" alignItems="center">
+        <Box
+          display="flex"
+          width="2.5rem"
+          height="2.5rem"
+          overflow="hidden"
+          borderRadius="xs"
+          alignItems="center"
+          position="relative"
+          justifyContent="center"
+        >
+          {isImageLoading && (
+            <Box position="absolute" top="-0.5rem">
+              <ProgressIndicator size={16} variant="loading" />
+            </Box>
+          )}
+          <img
+            src={img}
+            alt={name}
+            width="100%"
+            onLoad={() => setImageLoading(false)}
+          />
         </Box>
-        <img
-          src={img}
-          alt={name}
-          width="100%"
-          style={{ position: 'relative' }}
-        />
+        <Box
+          ml="1rem"
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+        >
+          <Typography variant="title" size="medium">
+            {name}
+          </Typography>
+        </Box>
       </Box>
       <Box
-        ml="1rem"
+        gap="2xs"
         display="flex"
+        alignItems="flex-end"
         flexDirection="column"
-        justifyContent="center"
       >
-        <Typography variant="title" size="medium">
-          {name}
+        <Typography variant="body" size="small">
+          {total} addresses
+        </Typography>
+        <Typography variant="body" size="small">
+          Last update: {new Date(updatedAt!).toLocaleDateString()}
         </Typography>
       </Box>
     </Box>
-    <Box gap="2xs" display="flex" alignItems="flex-end" flexDirection="column">
-      <Typography variant="body" size="small">
-        {total} addresses
-      </Typography>
-      <Typography variant="body" size="small">
-        Last update: {new Date(updatedAt!).toLocaleDateString()}
-      </Typography>
-    </Box>
-  </Box>
-);
+  );
+};
 
 export default NFTModalItem;
