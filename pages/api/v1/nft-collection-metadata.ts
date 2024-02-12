27a514f431abccad26d0838bbc48d90fa278837e
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2024-02-13 00:50:45 +0100

    * ✨ add new enforcer-machin

--- pages/api/v1/nft-collection-metadata.ts
+++ pages/api/v1/nft-collection-metadata.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
+import { fetchNftMetadata } from '@/api/indexer';
 import dbConnect from '@/server';
+import NFTCollection from '@/server/model/nft-collection';
 import NFTCollectionMetadata from '@/server/model/nft-collection-metadata';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -13,6 +15,33 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       if (!docs.length) return res.status(204).json([]);
 
       return res.status(200).json(docs);
+    }
+    if (req.method === 'POST') {
+      const collectionId = req.body.id;
+
+      const data = await fetchNftMetadata(collectionId);
+
+      if (!data) return res.status(500).send('Error fetching the data');
+
+      const meta = await NFTCollectionMetadata.findOne({ id: collectionId });
+
+      if (!meta)
+        await (
+          await NFTCollectionMetadata.create({ ...data, updatedAt: Date.now() })
+        ).save();
+
+      const holders = await NFTCollection.findOne({ collectionId });
+
+      if (!holders)
+        await (
+          await NFTCollection.create({
+            holders: [],
+            name: data.name,
+            collectionId: data.id,
+          })
+        ).save();
+
+      return res.status(200).send('Data created successfully');
     }
     return res.status(405).send('Method not allowed!');
   } catch (e) {
