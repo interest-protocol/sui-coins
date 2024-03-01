766ee3e73d94de6072c369c34fa16ed7a3cd7d88
KipandaJr <antoniokipandacardoso@gmail.com>
2024-03-01 23:19:07 +0100

    :sparkles: feat: New Tab Form

--- views/pool-details/index.tsx
+++ views/pool-details/index.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@interest-protocol/ui-kit';
 import { useRouter } from 'next/router';
 import { FC } from 'react';
 
@@ -10,6 +11,7 @@ import { TOKEN_ICONS } from '@/lib';
 import PoolTitleBar from '../components/pool-title-bar';
 import Error from '../error';
 import { PoolDetailsProps } from './pool-details.types';
+import PoolForm from './pool-form';
 
 const PoolDetails: FC<PoolDetailsProps> = ({ objectId }) => {
   const { push } = useRouter();
@@ -35,6 +37,24 @@ const PoolDetails: FC<PoolDetailsProps> = ({ objectId }) => {
           ''
         )}
       />
+      <Box
+        gap="xs"
+        mx="auto"
+        maxWidth="65rem"
+        flexDirection="column"
+        gridTemplateColumns="62% 38%"
+        display={['flex', 'flex', 'flex', 'grid']}
+      >
+        <PoolForm />
+        <Box
+          color="onSurface"
+          borderRadius="xs"
+          bg="lowestContainer"
+          p={['m', 'm', 'm', 'xl']}
+        >
+          Pool additional info
+        </Box>
+      </Box>
     </Layout>
   );
 };
