82942a25832c3037afec35d6373b9b5a7992527a
KipandaJr <antoniokipandacardoso@gmail.com>
2024-03-02 18:51:36 +0100

    :sparkles: feat: Form Deposit

--- views/pool-details/pool-form/component/receive-section/index.tsx
+++ views/pool-details/pool-form/component/receive-section/index.tsx
@@ -3,30 +3,28 @@ import { FC } from 'react';
 
 import { ReceiveSectionProps } from './receive-section.types';
 
-const PoolReceiveSection: FC<ReceiveSectionProps> = ({ symbol, balance }) => {
-  return (
-    <Box>
-      <Typography variant="body" size="large" mb="m">
-        You will receive (estimated):
-      </Typography>
-      <Box borderRadius="xs" bg="surface" py="xs">
-        <Box
-          py="xs"
-          px="m"
-          display="flex"
-          alignItems="center"
-          justifyContent="space-between"
-        >
-          <Typography variant="body" size="large">
-            {symbol}
-          </Typography>
-          <Typography variant="body" ml="m" size="large">
-            {balance}
-          </Typography>
-        </Box>
+const PoolReceiveSection: FC<ReceiveSectionProps> = ({ symbol, balance }) => (
+  <Box>
+    <Typography variant="body" size="large" mb="m">
+      You will receive (estimated):
+    </Typography>
+    <Box borderRadius="xs" bg="surface" py="xs">
+      <Box
+        py="xs"
+        px="m"
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Typography variant="body" size="large">
+          {symbol}
+        </Typography>
+        <Typography variant="body" ml="m" size="large">
+          {balance}
+        </Typography>
       </Box>
     </Box>
-  );
-};
+  </Box>
+);
 
 export default PoolReceiveSection;
