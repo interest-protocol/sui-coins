1cf6f76408c4040a1c221db5d2fb82e951b0815e
KipandaJr <antoniokipandacardoso@gmail.com>
2024-03-02 18:44:17 +0100

    :sparkles: feat: Form Deposit

--- views/pools/pools.types.ts
+++ views/pools/pools.types.ts
@@ -2,3 +2,26 @@ export enum PoolTabEnum {
   Pools,
   MyPosition,
 }
+
+import { TOKEN_SYMBOL } from '@/lib';
+
+export enum PoolOption {
+  Deposit,
+  Withdraw,
+}
+
+export interface CoinData {
+  decimals: number;
+  symbol: TOKEN_SYMBOL | string;
+  type: string;
+}
+
+export interface PoolToken extends CoinData {
+  value: string;
+  balance: number | null;
+}
+
+export interface PoolDepositForm {
+  tokenList: Array<PoolToken>;
+  lpCoin: string;
+}
