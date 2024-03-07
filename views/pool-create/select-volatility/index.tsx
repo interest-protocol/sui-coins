a9ec93a28738b9fdd4a5d23a26ae106702673554
git-marcopitra <marcomorete73@hotmail.com>
2024-03-07 22:33:16 +0100

    :sparkles: feat: add support to mulit info

---
+++ views/pool-create/select-volatility/index.tsx
@@ -0,0 +1,8 @@
+import { Box } from '@interest-protocol/ui-kit';
+import { FC } from 'react';
+
+const SelectVolatility: FC = () => {
+  return <Box color="white">Choose Volatility</Box>;
+};
+
+export default SelectVolatility;
