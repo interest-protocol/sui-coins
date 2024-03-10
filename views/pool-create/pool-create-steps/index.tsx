fa326794f9c5f67958424ff86ef32dbe17a629f7
KipandaJr <antoniokipandacardoso@gmail.com>
2024-03-10 01:09:26 +0100

    :bug: fix: step remove click event

--- views/pool-create/pool-create-steps/index.tsx
+++ views/pool-create/pool-create-steps/index.tsx
@@ -3,11 +3,7 @@ import { FC } from 'react';
 
 import { PoolCreateStepsProps } from './pool-create-steps.types';
 
-const PoolCreateSteps: FC<PoolCreateStepsProps> = ({
-  steps,
-  onStepClick,
-  currentStep,
-}) => (
+const PoolCreateSteps: FC<PoolCreateStepsProps> = ({ steps, currentStep }) => (
   <Box display="inline-flex" flexDirection="column" justifyContent="center">
     <Box display="flex" justifyContent="center" mb="s">
       {Array.from({ length: steps }).map((_, index) => (
@@ -15,11 +11,9 @@ const PoolCreateSteps: FC<PoolCreateStepsProps> = ({
           key={index}
           width="1.85rem"
           height="0.25rem"
-          cursor="pointer"
           marginRight="xs"
           borderRadius="xs"
           nLastChild={{ marginRight: 0 }}
-          onClick={() => onStepClick?.(index)}
           bg={currentStep >= index ? 'primary' : 'outlineVariant'}
           borderColor={currentStep !== index ? 'primary' : 'transparent'}
         />
