1ef4d934b05db70c5baa126f3fe0f85474dcf2c5
git-marcopitra <marcomorete73@hotmail.com>
2024-03-10 01:33:08 +0100

    :sparkles: feat: pool summary

---
+++ views/pool-create/pool-summary/index.tsx
@@ -0,0 +1,237 @@
+import { Box, Button, Typography } from '@interest-protocol/ui-kit';
+import { FC } from 'react';
+import { useFormContext } from 'react-hook-form';
+import { v4 } from 'uuid';
+
+import { TokenIcon } from '@/components';
+import { DexName } from '@/constants/pools';
+import { useNetwork } from '@/context/network';
+import { CircleCheckSVG } from '@/svg';
+import { formatMoney } from '@/utils';
+import { DEX_MAP } from '@/views/pools/pool-card/pool-card.data';
+
+import { CreatePoolForm } from '../pool-create.types';
+
+const PoolSummary: FC = () => {
+  const { network } = useNetwork();
+  const { getValues } = useFormContext<CreatePoolForm>();
+  const { type, isStable, tokens, dex } = getValues();
+
+  // TODO: add fee
+  const fee = 0;
+
+  return (
+    <Box
+      my="xl"
+      p="xl"
+      mx="auto"
+      gap="2rem"
+      bg="container"
+      borderRadius="xs"
+      maxWidth="27.25rem"
+    >
+      <Typography
+        mb="3xl"
+        size="large"
+        variant="label"
+        color="onSurface"
+        textAlign="center"
+      >
+        Pool summary
+      </Typography>
+      <Box
+        my="m"
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Box display="flex" gap="s">
+          <Box color="success">
+            <CircleCheckSVG maxWidth="1.2rem" maxHeight="1.2rem" width="100%" />
+          </Box>
+          <Typography
+            size="medium"
+            variant="body"
+            opacity={0.64}
+            color="onSurface"
+          >
+            Pool Type
+          </Typography>
+        </Box>
+        <Box
+          px="m"
+          py="xs"
+          display="flex"
+          borderRadius="xs"
+          bg="lowContainer"
+          color="onSurface"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          {type}
+        </Box>
+      </Box>
+      <Box
+        my="m"
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Box display="flex" gap="s">
+          <Box color="success">
+            <CircleCheckSVG maxWidth="1.2rem" maxHeight="1.2rem" width="100%" />
+          </Box>
+          <Typography
+            size="medium"
+            variant="body"
+            opacity={0.64}
+            color="onSurface"
+          >
+            Pool Algorithm
+          </Typography>
+        </Box>
+        <Box
+          px="m"
+          py="xs"
+          display="flex"
+          borderRadius="xs"
+          bg="lowContainer"
+          color="onSurface"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          {isStable ? 'Stable' : 'Volatile'}
+        </Box>
+      </Box>
+      <Box
+        my="m"
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Box display="flex" gap="s">
+          <Box color="success">
+            <CircleCheckSVG maxWidth="1.2rem" maxHeight="1.2rem" width="100%" />
+          </Box>
+          <Typography
+            size="medium"
+            variant="body"
+            opacity={0.64}
+            color="onSurface"
+          >
+            Coins in Pool
+          </Typography>
+        </Box>
+        <Box
+          px="m"
+          py="xs"
+          display="flex"
+          borderRadius="xs"
+          bg="lowContainer"
+          color="onSurface"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          {tokens?.length}
+        </Box>
+      </Box>
+      <Box
+        p="s"
+        gap="s"
+        display="flex"
+        bg="lowContainer"
+        color="onSurface"
+        borderRadius="xs"
+        flexDirection="column"
+      >
+        {tokens?.map(({ symbol, value }) => (
+          <Box
+            key={v4()}
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Box display="flex" alignItems="center" gap="s">
+              <Box
+                display="flex"
+                width="2.5rem"
+                height="2.5rem"
+                borderRadius="xs"
+                alignItems="center"
+                bg="lowestContainer"
+                justifyContent="center"
+              >
+                <TokenIcon tokenId={symbol} network={network} />
+              </Box>
+              <Typography variant="body" size="medium">
+                {symbol}
+              </Typography>
+            </Box>
+            <Box>{formatMoney(Number(value))}</Box>
+          </Box>
+        ))}
+      </Box>
+      <Box
+        my="m"
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Box display="flex" gap="s">
+          <Box color="success">
+            <CircleCheckSVG maxWidth="1.2rem" maxHeight="1.2rem" width="100%" />
+          </Box>
+          <Typography
+            size="medium"
+            variant="body"
+            opacity={0.64}
+            color="onSurface"
+          >
+            Dex
+          </Typography>
+        </Box>
+        <Box
+          px="m"
+          py="xs"
+          display="flex"
+          borderRadius="xs"
+          bg="lowContainer"
+          color="onSurface"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          {DEX_MAP[dex as DexName]?.name}
+        </Box>
+      </Box>
+
+      <Box
+        p="s"
+        gap="s"
+        display="flex"
+        color="onSurface"
+        borderRadius="xs"
+        bg="lowestContainer"
+        justifyContent="space-between"
+      >
+        <Typography variant="body" size="small">
+          Pool Creation Fee:
+        </Typography>
+        <Typography variant="body" size="small">
+          {fee} Sui
+        </Typography>
+      </Box>
+      <Box display="flex" justifyContent="center" gap="s" mt="xl">
+        <Button
+          variant="outline"
+          color="onSurface"
+          borderColor="outlineVariant"
+        >
+          Cancel
+        </Button>
+        <Button variant="filled">Create Pool</Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default PoolSummary;
