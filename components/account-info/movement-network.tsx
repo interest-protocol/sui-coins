e2462bd69c425b0b717ca180d1648dd65a591f05
Atila <atilaesousa@gmail.com>
2024-03-12 16:21:49 +0100

    :bug: fix: using enum in disable

--- components/account-info/movement-network.tsx
+++ components/account-info/movement-network.tsx
@@ -112,7 +112,7 @@ const MovementNetwork: FC = () => {
             <OptionItem
               key={v4()}
               selected={networkKey === network}
-              disabled={networkKey === 'm2:testnet'}
+              disabled={networkKey === Network.TESTNET}
               onClick={() => changeNetwork(networkKey as Network)}
             >
               <MovementLogoSVG maxWidth="2rem" maxHeight="2rem" />
