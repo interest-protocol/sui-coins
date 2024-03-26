d02084cbe6b82195439a2872b8ca169ef7dde41a
Jose Cerqueira <josecerqueira@protonmail.com>
2024-03-26 14:49:30 +0000

    :bug: fix: use objectref instead of object

--- utils/tx/tx.types.ts
+++ utils/tx/tx.types.ts
@@ -1,6 +1,6 @@
 import { useSignTransactionBlock } from '@mysten/dapp-kit/dist/esm';
 import { SuiClient } from '@mysten/sui.js/client';
-import { SuiTransactionBlockResponseOptions } from '@mysten/sui.js/src/client';
+import { SuiTransactionBlockResponseOptions } from '@mysten/sui.js/client';
 import { TransactionBlock } from '@mysten/sui.js/transactions';
 import { WalletAccount } from '@wallet-standard/base';
 
