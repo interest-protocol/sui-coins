79303abbcacabef52f23b3b332c5d34e2bb0c8d0
Mario Batalha <mariobatalha9@gmail.com>
2024-04-03 15:32:17 +0100

    :recycle: refactor: color theme

--- views/airdrop/airdrop-preview-modal.tsx
+++ views/airdrop/airdrop-preview-modal.tsx
@@ -1,9 +1,11 @@
-import { Box, Modal, Typography } from '@interest-protocol/ui-kit';
+import { Box, Button, Modal, Typography } from '@interest-protocol/ui-kit';
 import BigNumber from 'bignumber.js';
+import { useRouter } from 'next/router';
 import { FC } from 'react';
 import { useFormContext, useWatch } from 'react-hook-form';
 
 import { TokenIcon } from '@/components';
+import { Routes, RoutesEnum } from '@/constants';
 import { Network } from '@/constants/network';
 import { useNetwork } from '@/context/network';
 import { FixedPointMath } from '@/lib';
@@ -22,6 +24,7 @@ const AirdropPreviewModal: FC<AirdropPreviewModalProps> = ({
 }) => {
   const network = useNetwork();
   const { control } = useFormContext<IAirdropForm>();
+  const { push } = useRouter();
 
   const { symbol, decimals, type } = useWatch({ control, name: 'token' });
   const usdPrice = useWatch({ control, name: 'tokenUSDPrice' });
@@ -37,20 +40,25 @@ const AirdropPreviewModal: FC<AirdropPreviewModalProps> = ({
       )
     : 0;
 
+  const handleGoBackToAirdrop = () => {
+    push(Routes[RoutesEnum.Airdrop]);
+  };
+
   return (
     <Modal custom isOpen={isOpen}>
       <Box
         maxWidth="100%"
+        maxHeight="90vh"
         borderRadius="xs"
         width="26.875rem"
         minHeight="30rem"
-        maxHeight="90vh"
+        color="onSurface"
         alignItems="center"
         display="inline-flex"
-        justifyContent="space-between"
+        bg="lowestContainer"
         flexDirection="column"
         boxShadow="dropShadow.2xl"
-        backgroundColor="container"
+        justifyContent="space-between"
       >
         <Box width="100%">
           <Box
@@ -58,19 +66,16 @@ const AirdropPreviewModal: FC<AirdropPreviewModalProps> = ({
             display="flex"
             alignItems="center"
             justifyContent="space-between"
-            color="onSurface"
           >
-            <ArrowLeftSVG maxWidth="1.5rem" maxHeight="1.5rem" width="100%" />
+            <Button variant="text" isIcon onClick={handleGoBackToAirdrop}>
+              <ArrowLeftSVG maxWidth="1.5rem" maxHeight="1.5rem" width="100%" />
+            </Button>
             <Typography size="large" variant="title">
               Airdrop
             </Typography>
-            <TimesSVG
-              onClick={onClose}
-              width="100%"
-              maxWidth="1rem"
-              cursor="pointer"
-              maxHeight="1rem"
-            />
+            <Button variant="text" isIcon onClick={onClose}>
+              <TimesSVG maxWidth="1rem" maxHeight="1rem" width="100%" />
+            </Button>
           </Box>
           <Box
             px="xl"
@@ -85,11 +90,11 @@ const AirdropPreviewModal: FC<AirdropPreviewModalProps> = ({
             </Typography>
             <Box
               p="s"
-              bg="lowestContainer"
+              display="flex"
               color="onSurface"
               borderRadius="xs"
-              display="flex"
               alignItems="center"
+              bg="surface"
               justifyContent="space-between"
             >
               <Box display="flex" alignItems="center" gap="m">
