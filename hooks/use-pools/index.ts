8f98060aa4f46274e8282e453fdc9cd533525046
git-marcopitra <marcomorete73@hotmail.com>
2024-04-15 03:18:53 +0100

    :bug: fix: render information on render

--- hooks/use-pools/index.ts
+++ hooks/use-pools/index.ts
@@ -11,60 +11,54 @@ import { getPoolCoinTypes } from '@/utils/pool';
 
 import { useMovementClient } from '../use-movement-client';
 
-const parsePool = (x: SuiObjectResponse): AmmPool => {
-  return {
-    poolId: STATE_KEY_TO_POOL_ID[pathOr('', ['id', 'id'], x)],
-    stateId: pathOr('', ['id', 'id'], x),
-    adminBalanceX: BigNumber(
-      pathOr('0', ['value', 'fields', 'admin_balance_x'], x)
+const parsePool = (x: SuiObjectResponse): AmmPool => ({
+  poolId: STATE_KEY_TO_POOL_ID[pathOr('', ['id', 'id'], x)],
+  stateId: pathOr('', ['id', 'id'], x),
+  adminBalanceX: BigNumber(
+    pathOr('0', ['value', 'fields', 'admin_balance_x'], x)
+  ),
+  adminBalanceY: BigNumber(
+    pathOr('0', ['value', 'fields', 'admin_balance_y'], x)
+  ),
+  balanceX: BigNumber(pathOr('0', ['value', 'fields', 'balance_x'], x)),
+  balanceY: BigNumber(pathOr('0', ['value', 'fields', 'balance_y'], x)),
+  decimalsX: BigNumber(pathOr('0', ['value', 'fields', 'decimals_x'], x)).e!,
+  decimalsY: BigNumber(pathOr('0', ['value', 'fields', 'decimals_y'], x)).e!,
+  fees: {
+    feeIn: BigNumber(
+      pathOr('0', ['value', 'fields', 'fees', 'fields', 'fee_in_percent'], x)
     ),
-    adminBalanceY: BigNumber(
-      pathOr('0', ['value', 'fields', 'admin_balance_y'], x)
+    feeOut: BigNumber(
+      pathOr('0', ['value', 'fields', 'fees', 'fields', 'fee_out_percent'], x)
     ),
-    balanceX: BigNumber(pathOr('0', ['value', 'fields', 'balance_x'], x)),
-    balanceY: BigNumber(pathOr('0', ['value', 'fields', 'balance_y'], x)),
-    decimalsX: BigNumber(pathOr('0', ['value', 'fields', 'decimals_x'], x)).e!,
-    decimalsY: BigNumber(pathOr('0', ['value', 'fields', 'decimals_y'], x)).e!,
-    fees: {
-      feeIn: BigNumber(
-        pathOr('0', ['value', 'fields', 'fees', 'fields', 'fee_in_percent'], x)
-      ),
-      feeOut: BigNumber(
-        pathOr('0', ['value', 'fields', 'fees', 'fields', 'fee_out_percent'], x)
-      ),
-      adminFee: BigNumber(
-        pathOr(
-          '0',
-          ['value', 'fields', 'fees', 'fields', 'admin_fee_percent'],
-          x
-        )
-      ),
-    },
-    lpCoinSupply: BigNumber(
-      pathOr(
-        '0',
-        [
-          'value',
-          'fields',
-          'lp_coin_cap',
-          'fields',
-          'total_supply',
-          'fields',
-          'value',
-        ],
-        x
-      )
+    adminFee: BigNumber(
+      pathOr('0', ['value', 'fields', 'fees', 'fields', 'admin_fee_percent'], x)
     ),
-    type: pathOr('', ['value', 'type'], x),
-    coinTypes: getPoolCoinTypes(pathOr('', ['value', 'type'], x)),
-    poolType: PoolTypeEnum.amm,
-    isVolatile: pathOr(
-      true,
-      ['value', 'fields', 'fees', 'fields', 'volatile'],
+  },
+  lpCoinSupply: BigNumber(
+    pathOr(
+      '0',
+      [
+        'value',
+        'fields',
+        'lp_coin_cap',
+        'fields',
+        'total_supply',
+        'fields',
+        'value',
+      ],
       x
-    ),
-  };
-};
+    )
+  ),
+  type: pathOr('', ['value', 'type'], x),
+  coinTypes: getPoolCoinTypes(pathOr('', ['value', 'type'], x)),
+  poolType: PoolTypeEnum.amm,
+  isVolatile: pathOr(
+    true,
+    ['value', 'fields', 'fees', 'fields', 'volatile'],
+    x
+  ),
+});
 
 export const usePool = (parentId: string) => {
   const client = useMovementClient();
