4d805088c9f0353db7eb19bf8d59f5059c85cebd
git-marcopitra <marcomorete73@hotmail.com>
2024-04-21 12:12:39 +0100

    :green_heart: build: fix build bug

--- views/pools/header/find-pool-button.tsx
+++ views/pools/header/find-pool-button.tsx
@@ -22,8 +22,20 @@ const FinPoolButton: FC = () => {
 
   const openModal = () => {
     form.setValue('tokenList', [
-      { symbol: '', type: '', decimals: 0, value: '', locked: false },
-      { symbol: '', type: '', decimals: 0, value: '', locked: false },
+      {
+        symbol: '',
+        type: '' as `0x${string}`,
+        decimals: 0,
+        value: '',
+        locked: false,
+      },
+      {
+        symbol: '',
+        type: '' as `0x${string}`,
+        decimals: 0,
+        value: '',
+        locked: false,
+      },
     ]);
 
     setModal(
