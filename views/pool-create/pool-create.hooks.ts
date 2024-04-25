01a073446ccf05ad4e1ad6f556cc775a3db9b76f
git-marcopitra <marcomorete73@hotmail.com>
2024-04-25 09:35:30 +0100

    :bug: fix: merge coins

--- views/pool-create/pool-create.hooks.ts
+++ views/pool-create/pool-create.hooks.ts
@@ -1,10 +1,14 @@
 import { CLAMM, PRECISION } from '@interest-protocol/clamm';
 import { useCurrentAccount } from '@mysten/dapp-kit';
+import { TransactionBlock } from '@mysten/sui.js/dist/cjs/transactions';
+
+import { useWeb3 } from '@/hooks/use-web3';
 
 import { Token } from './pool-create.types';
 import { getLpCoin } from './pool-create.utils';
 
 export const useCreateStablePool = () => {
+  const { coinsMap } = useWeb3();
   const currentAccount = useCurrentAccount();
 
   return async (tokens: ReadonlyArray<Token>) => {
@@ -17,10 +21,20 @@ export const useCreateStablePool = () => {
 
     if (!treasuryCap) throw new Error('No authorization to use this LP coin');
 
+    const mergeTxb = new TransactionBlock();
+
+    const coins = tokens.map(({ type }) =>
+      mergeTxb.mergeCoins(
+        coinsMap[type].coinObjectId,
+        coinsMap[type].objects.slice(1).map((object) => object.coinObjectId)
+      )
+    );
+
     const typeArguments = [...tokens.map((token) => token.type), coinType];
 
     const { pool, poolAdmin, lpCoin, txb } = await CLAMM.newStable({
-      coins: tokens.map((token) => token.type),
+      coins,
+      txb: mergeTxb,
       lpCoinTreasuryCap: treasuryCap,
       typeArguments: typeArguments,
     });
@@ -32,6 +46,7 @@ export const useCreateStablePool = () => {
 };
 
 export const useCreateVolatilePool = () => {
+  const { coinsMap } = useWeb3();
   const currentAccount = useCurrentAccount();
 
   return async (tokens: ReadonlyArray<Token>) => {
@@ -44,12 +59,22 @@ export const useCreateVolatilePool = () => {
 
     if (!treasuryCap) throw new Error('No authorization to use this LP coin');
 
+    const mergeTxb = new TransactionBlock();
+
+    const coins = tokens.map(({ type }) =>
+      mergeTxb.mergeCoins(
+        coinsMap[type].coinObjectId,
+        coinsMap[type].objects.slice(1).map((object) => object.coinObjectId)
+      )
+    );
+
     const typeArguments = [...tokens.map((token) => token.type), coinType];
 
     const price = BigInt(+tokens[0].value / +tokens[1].value);
 
     const { pool, poolAdmin, lpCoin, txb } = await CLAMM.newVolatile({
-      coins: tokens.map((token) => token.type),
+      coins,
+      txb: mergeTxb,
       lpCoinTreasuryCap: treasuryCap,
       typeArguments: typeArguments,
       prices: [price * PRECISION],
