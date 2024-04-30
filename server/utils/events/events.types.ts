88c00978ae004ad77c9b21ab8138e276e9725267
Jose Cerqueira <josecerqueira@protonmail.com>
2024-04-30 19:57:14 +0100

    :wrench: fix: import statement

--- server/utils/events/events.types.ts
+++ server/utils/events/events.types.ts
@@ -1,6 +1,6 @@
 import { SuiClient } from '@mysten/sui.js/client';
 import { EventId, SuiEvent } from '@mysten/sui.js/client';
-import { SuiEventFilter } from '@mysten/sui.js/src/client';
+import { SuiEventFilter } from '@mysten/sui.js/client';
 
 type SuiEventsCursor = EventId | null | undefined;
 
