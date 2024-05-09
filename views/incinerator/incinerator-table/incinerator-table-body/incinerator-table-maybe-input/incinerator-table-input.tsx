c800b3056b661d1eede028489fdd709643c7cdcf
KipandaJr <antoniokipandacardoso@gmail.com>
2024-05-09 22:53:37 +0100

    :sparkles: feat: button max added on field

--- views/incinerator/incinerator-table/incinerator-table-body/incinerator-table-maybe-input/incinerator-table-input.tsx
+++ views/incinerator/incinerator-table/incinerator-table-body/incinerator-table-maybe-input/incinerator-table-input.tsx
@@ -1,4 +1,4 @@
-import { TextField } from '@interest-protocol/ui-kit';
+import { Button, TextField } from '@interest-protocol/ui-kit';
 import {
   type FC,
   type KeyboardEventHandler,
@@ -9,6 +9,7 @@ import { useFormContext, useWatch } from 'react-hook-form';
 
 import { Keys } from '@/constants';
 import type { CoinObject } from '@/hooks/use-get-all-coins/use-get-all-coins.types';
+import { useWeb3 } from '@/hooks/use-web3';
 import { FixedPointMath } from '@/lib';
 import { parseInputEventToNumberString, ZERO_BIG_NUMBER } from '@/utils';
 
@@ -19,10 +20,18 @@ import type {
 import IncineratorTableRowActionButton from './incinerator-table-actions';
 
 const IncineratorTableInput: FC<IncineratorTableRowProps> = ({ index }) => {
+  const { coinsMap } = useWeb3();
   const { control, setValue, getValues } = useFormContext<IncineratorForm>();
 
   const display = useWatch({ control, name: `objects.${index}.display` });
   const [oldValue, setOldValue] = useState(getValues(`objects.${index}.value`));
+
+  const balance = coinsMap[`${display?.type}`]
+    ? FixedPointMath.toNumber(
+        coinsMap[`${display?.type}`].balance,
+        coinsMap[`${display?.type}`].decimals
+      )
+    : 0;
 
   const handleApprove: MouseEventHandler<HTMLDivElement> = (e) => {
     e?.stopPropagation();
@@ -39,6 +48,12 @@ const IncineratorTableInput: FC<IncineratorTableRowProps> = ({ index }) => {
     setValue(`objects.${index}.value`, oldValue);
 
     setValue(`objects.${index}.isEditing`, false);
+  };
+
+  const handleMax: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e?.stopPropagation();
+
+    setValue(`objects.${index}.value`, `${balance}`);
   };
 
   const onKeyDown: KeyboardEventHandler<HTMLInputElement> = (e) => {
@@ -59,6 +74,17 @@ const IncineratorTableInput: FC<IncineratorTableRowProps> = ({ index }) => {
           borderRadius: 'xs',
           bg: 'lowestContainer',
         }}
+        Suffix={
+          <Button
+            p="xs"
+            type="button"
+            mr="-0.75rem"
+            variant="text"
+            onClick={handleMax}
+          >
+            max
+          </Button>
+        }
         onChange={(e) =>
           setValue(
             `objects.${index}.value`,
