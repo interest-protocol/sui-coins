1f5bc3516222ff081a096d2cbc1ff52179d8832c
Átila e Sousa <52140947+atilasousa@users.noreply.github.com>
2024-05-10 16:36:38 +0100

    ✨ meme chan (#254)

--- components/layout/sidebar/menu-list.tsx
+++ components/layout/sidebar/menu-list.tsx
@@ -1,8 +1,10 @@
-import { Box } from '@interest-protocol/ui-kit';
+import { Box, Typography } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
 import { v4 } from 'uuid';
 
+import { PARTNERS } from '@/constants';
 import { useNetwork } from '@/context/network';
+import { ArrowTopRightSVG } from '@/svg';
 
 import SideBarMenuItem from './menu-item';
 import { SIDEBAR_ITEMS } from './sidebar.data';
@@ -16,19 +18,92 @@ const SidebarMenuList: FC<MenuListProps> = ({
   const network = useNetwork();
 
   return (
-    <Box display="flex" flexDirection="column" gap="s">
-      {SIDEBAR_ITEMS.filter(({ networks }) => networks.includes(network)).map(
-        (item) => (
-          <SideBarMenuItem
-            {...item}
-            key={v4()}
-            isCollapsed={isCollapsed}
-            setIsCollapsed={setIsCollapsed}
-            setTemporarilyOpen={setTemporarilyOpen}
-          />
-        )
-      )}
-    </Box>
+    <>
+      <Box display="flex" flexDirection="column" gap="s" pb="m">
+        {SIDEBAR_ITEMS.filter(({ networks }) => networks.includes(network)).map(
+          (item) => (
+            <SideBarMenuItem
+              {...item}
+              key={v4()}
+              isCollapsed={isCollapsed}
+              setIsCollapsed={setIsCollapsed}
+              setTemporarilyOpen={setTemporarilyOpen}
+            />
+          )
+        )}
+      </Box>
+      <Box borderTop="1px solid" borderColor="outlineVariant" />
+      {PARTNERS.map(({ link, name, img }) => (
+        <Box pt="m" key={v4()}>
+          <a href={link} target="_blank" rel="noreferrer">
+            {isCollapsed ? (
+              <Box
+                display="flex"
+                borderRadius="s"
+                cursor="pointer"
+                alignItems="center"
+                bg="lowestContainer"
+                justifyContent="center"
+              >
+                <Box
+                  display="flex"
+                  width="2.8rem"
+                  height="2.8rem"
+                  overflow="hidden"
+                  borderRadius="50%"
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <img src={img} alt={name} width="80%" height="80%" />
+                </Box>
+              </Box>
+            ) : (
+              <Box
+                p="xs"
+                pr="s"
+                display="flex"
+                borderRadius="s"
+                cursor="pointer"
+                alignItems="center"
+                bg="lowestContainer"
+                justifyContent="space-between"
+              >
+                <Box display="flex" alignItems="center" justifyItems="center">
+                  <Box mr="s">
+                    <Box
+                      display="flex"
+                      width="2.8rem"
+                      height="2.8rem"
+                      overflow="hidden"
+                      borderRadius="50%"
+                      alignItems="center"
+                      justifyContent="center"
+                    >
+                      <img src={img} alt={name} width="80%" height="80%" />
+                    </Box>
+                  </Box>
+                  <Typography
+                    size="large"
+                    variant="label"
+                    textDecoration="underline"
+                    display={isCollapsed ? 'none' : 'block'}
+                  >
+                    {name}
+                  </Typography>
+                </Box>
+                <Box display={isCollapsed ? 'none' : 'block'}>
+                  <ArrowTopRightSVG
+                    width="1.5rem"
+                    maxWidth="1.5rem"
+                    maxHeight="1.5rem"
+                  />
+                </Box>
+              </Box>
+            )}
+          </a>
+        </Box>
+      ))}
+    </>
   );
 };
 
