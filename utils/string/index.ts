ef3e3d19ef37f828fa134bee43fb07f3dadffba1
git-marcopitra <marcomorete73@hotmail.com>
2024-05-10 07:29:42 +0100

    :green_heart: build: fix: importing ts

--- utils/string/index.ts
+++ utils/string/index.ts
@@ -1,7 +1,7 @@
 import { ChangeEvent } from 'react';
 
+import { ObjectData } from '@/components/web3-manager/all-objects-manager/all-objects.types';
 import { MAX_NUMBER_INPUT_VALUE } from '@/constants';
-import { ObjectData } from '@/hooks/use-all-objects/all-objects.types';
 
 const isExponential = (number: number) => number.toString().includes('e');
 
