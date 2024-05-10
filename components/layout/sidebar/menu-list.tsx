942c5d9809a293e706e4cbebe48d4517449bc210
git-marcopitra <marcomorete73@hotmail.com>
2024-05-10 16:05:25 +0100

    :green_heart: build: fix: importing ts

--- components/layout/sidebar/menu-list.tsx
+++ components/layout/sidebar/menu-list.tsx
@@ -2,8 +2,9 @@ import { Box, Typography } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
 import { v4 } from 'uuid';
 
+import { PARTNERS } from '@/constants';
 import { useNetwork } from '@/context/network';
-import { ArrowTopRightSVG, MemechanSVG } from '@/svg';
+import { ArrowTopRightSVG } from '@/svg';
 
 import SideBarMenuItem from './menu-item';
 import { SIDEBAR_ITEMS } from './sidebar.data';
@@ -31,43 +32,41 @@ const SidebarMenuList: FC<MenuListProps> = ({
           )
         )}
       </Box>
-      <hr />
-      <Box pt="m">
-        <a href="https://www.memechan.gg/" target="_blank" rel="noreferrer">
-          <Box
-            bg="white"
-            display="flex"
-            justifyContent="space-between"
-            alignItems="center"
-            p="xs"
-            pr="s"
-            borderRadius="s"
-            cursor="pointer"
-          >
-            <Box display="flex" alignItems="center" justifyItems="center">
-              <Box mr="s">
-                <MemechanSVG
-                  maxWidth="2.8rem"
-                  maxHeight="2.8rem"
-                  width="100%"
-                />
+      <Box as="hr" borderColor="outline" />
+      {PARTNERS.map(({ link, name, Icon }) => (
+        <Box pt="m" key={v4()}>
+          <a href={link} target="_blank" rel="noreferrer">
+            <Box
+              bg="white"
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              p="xs"
+              pr="s"
+              borderRadius="s"
+              cursor="pointer"
+            >
+              <Box display="flex" alignItems="center" justifyItems="center">
+                <Box mr="s">
+                  <Icon maxWidth="2.8rem" maxHeight="2.8rem" width="100%" />
+                </Box>
+                <Typography
+                  textDecoration="underline"
+                  variant="label"
+                  size="large"
+                >
+                  {name}
+                </Typography>
               </Box>
-              <Typography
-                textDecoration="underline"
-                variant="label"
-                size="large"
-              >
-                MEMECHAN.ORG
-              </Typography>
+              <ArrowTopRightSVG
+                maxWidth="1.5rem"
+                maxHeight="1.5rem"
+                width="1.5rem"
+              />
             </Box>
-            <ArrowTopRightSVG
-              maxWidth="1.5rem"
-              maxHeight="1.5rem"
-              width="1.5rem"
-            />
-          </Box>
-        </a>
-      </Box>
+          </a>
+        </Box>
+      ))}
     </>
   );
 };
---
+++ constants/partners.ts
@@ -0,0 +1,9 @@
+import { MemechanSVG } from '@/svg';
+
+export const PARTNERS = [
+  {
+    link: 'https://www.memechan.gg/',
+    Icon: MemechanSVG,
+    name: 'MEMECHAN.ORG',
+  },
+];
