0207209f97279517358e7ee855be25ff22416d70
Jose Cerqueira <josecerqueira@protonmail.com>
2024-05-11 03:08:36 +0100

    :sparkles: feat: improve pool details

--- hooks/use-clamm-sdk/index.tsx
+++ hooks/use-clamm-sdk/index.tsx
@@ -2,7 +2,6 @@ import { CLAMM } from '@interest-protocol/clamm-sdk';
 import { useSuiClient } from '@mysten/dapp-kit';
 
 import { Network } from '@/constants';
-import { CLAMM_PACKAGE_ADDRESSES } from '@/constants/dex';
 import { useNetwork } from '@/context/network';
 
 let cachedClamm: CLAMM;
@@ -15,8 +14,6 @@ export const useClammSdk = (): CLAMM => {
     cachedClamm ??
     new CLAMM({
       suiClient,
-      packageAddress: CLAMM_PACKAGE_ADDRESSES[network].CLAMM,
-      suiTearsAddress: CLAMM_PACKAGE_ADDRESSES[network].SUITEARS,
       network: network === Network.MAINNET ? 'mainnet' : 'testnet',
     });
 
