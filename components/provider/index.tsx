6f58c21f7cd704aa6cc353a9f10dc627d893110a
git-marcopitra <marcomorete73@hotmail.com>
2024-05-13 18:34:35 +0100

    :green_heart: build: fix: importing ts

--- components/provider/index.tsx
+++ components/provider/index.tsx
@@ -15,7 +15,7 @@ const Provider: FC<PropsWithChildren> = ({ children }) => (
   <ThemeManager>
     <QueryClientProvider client={queryClient}>
       <NetworkProvider>
-        <WalletProvider autoConnect zkSend={{ name: 'Sui Coins' }}>
+        <WalletProvider autoConnect stashedWallet={{ name: 'Sui Coins' }}>
           <SuiNsProvider>{children}</SuiNsProvider>
         </WalletProvider>
       </NetworkProvider>
