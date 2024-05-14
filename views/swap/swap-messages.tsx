60ae129ce68ef894c9b52cc7de9444eeaec51e57
KipandaJr <antoniokipandacardoso@gmail.com>
2024-05-14 13:53:01 +0100

    :lipstick: feat: change box color error message

--- views/swap/swap-messages.tsx
+++ views/swap/swap-messages.tsx
@@ -1,13 +1,19 @@
-import { Box, Typography } from '@interest-protocol/ui-kit';
+import { Box, Theme, Typography, useTheme } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
 import { useWatch } from 'react-hook-form';
 
 import { DotErrorSVG } from '@/svg';
 
+import { SwapMessagesEnum } from './swap.data';
+
 const SwapMessages: FC = () => {
+  const { colors } = useTheme() as Theme;
+
   const { control } = useWatch();
 
   const error = useWatch({ control, name: 'error' });
+
+  const isLeastOneSuiErrorMessage = SwapMessagesEnum.leastOneSui == error;
 
   if (!error) return null;
 
@@ -19,11 +25,18 @@ const SwapMessages: FC = () => {
       display="flex"
       borderRadius="xs"
       border="1px solid"
-      bg="errorContainer"
-      color="onErrorContainer"
-      borderColor="onErrorContainer"
+      bg={isLeastOneSuiErrorMessage ? 'lowContainer' : 'errorContainer'}
+      color={isLeastOneSuiErrorMessage ? 'outline' : 'onErrorContainer'}
+      borderColor={isLeastOneSuiErrorMessage ? 'outline' : 'onErrorContainer'}
     >
-      <DotErrorSVG maxHeight="1rem" maxWidth="1rem" width="100%" />
+      <DotErrorSVG
+        dotColor={
+          isLeastOneSuiErrorMessage ? colors.lowContainer : colors.error
+        }
+        maxHeight="1rem"
+        maxWidth="1rem"
+        width="100%"
+      />
       <Typography variant="label" size="medium">
         {error}
       </Typography>
