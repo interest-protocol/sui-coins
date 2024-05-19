9ae84eef8278d7bc6fc5cefc01684339df0afb20
Jose Cerqueira <josecerqueira@protonmail.com>
2024-05-19 07:40:56 +0100

    :wrench: fix: pool details loading accordion

--- views/pool-details/pool-info/pool-info-loading.tsx
+++ views/pool-details/pool-info/pool-info-loading.tsx
@@ -2,21 +2,13 @@ import { Box } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
 import { v4 } from 'uuid';
 
-import Accordion from './components/accordion';
 import PoolDetailsCollapseItemSkeleton from './components/accordion/item-standard-skeleton';
 
 const PoolInfoLoading: FC = () => (
-  <Box>
-    <Accordion loading>
-      {Array.from({ length: 5 }).map(() => (
-        <PoolDetailsCollapseItemSkeleton key={v4()} />
-      ))}
-    </Accordion>
-    <Accordion loading>
-      {Array.from({ length: 2 }).map(() => (
-        <PoolDetailsCollapseItemSkeleton key={v4()} />
-      ))}
-    </Accordion>
+  <Box m="1rem">
+    {Array.from({ length: 7 }).map(() => (
+      <PoolDetailsCollapseItemSkeleton key={v4()} />
+    ))}
   </Box>
 );
 
