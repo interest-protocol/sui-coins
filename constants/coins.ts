78f127fd6ff5799298797f2454f52924e5b616a9
git-marcopitra <marcomorete73@hotmail.com>
2024-05-31 09:34:58 +0100

    :sparkles: feat: add USDT

--- constants/coins.ts
+++ constants/coins.ts
@@ -55,6 +55,11 @@ export const FAUCET_COINS = [
     symbol: 'USDC',
     type: USDC_TYPE,
   },
+  {
+    decimals: 6,
+    symbol: 'USDT',
+    type: USDT_TYPE,
+  },
 ];
 
 export const COINS = [
