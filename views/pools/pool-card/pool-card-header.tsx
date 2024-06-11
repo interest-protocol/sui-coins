48db9c572673343d660914546f06b0f699324986
KipandaJr <antoniokipandacardoso@gmail.com>
2024-06-11 02:48:53 +0100

    :sparkles: feat: adding icon on pool card header

--- views/pools/pool-card/pool-card-header.tsx
+++ views/pools/pool-card/pool-card-header.tsx
@@ -4,27 +4,44 @@ import { v4 } from 'uuid';
 
 import { ArrowObliqueSVG } from '@/svg';
 
-import { PoolCardHeaderProps } from './pool-card.types';
+import { HeaderCardTagIcon } from './pool-card.data';
+import { FormFilterValue, PoolCardHeaderProps } from './pool-card.types';
 
 const PoolCardHeader: FC<PoolCardHeaderProps> = ({ tags }) => (
   <Box display="flex" alignItems="center" justifyContent="space-between">
     <Box display="flex" flexWrap="wrap" alignItems="center">
-      {tags?.map((tag) => (
-        <Tag
-          key={v4()}
-          px="0"
-          py="0"
-          mb="2xs"
-          mr="2xs"
-          size="small"
-          height="1.4rem"
-          variant="outline"
-        >
-          <Typography size="small" variant="label">
-            {tag}
-          </Typography>
-        </Tag>
-      ))}
+      {tags?.map((tag) => {
+        const TagIcon =
+          HeaderCardTagIcon[
+            tag as
+              | 'AMM'
+              | 'CLAMM'
+              | FormFilterValue.stable
+              | FormFilterValue.volatile
+          ];
+        return (
+          <Tag
+            key={v4()}
+            px="0"
+            py="0"
+            mb="2xs"
+            mr="2xs"
+            size="small"
+            height="1.4rem"
+            variant="outline"
+          >
+            <Box display="flex" gap="xs">
+              <Box width="1rem" height="1rem" display="flex">
+                <TagIcon maxWidth="100%" maxHeight="100%" width="100%" />
+              </Box>
+
+              <Typography size="small" variant="label">
+                {tag}
+              </Typography>
+            </Box>
+          </Tag>
+        );
+      })}
     </Box>
     <Button
       mb="auto"
