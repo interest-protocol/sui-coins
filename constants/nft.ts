693e169a6719b0a21c8c218bb86807b916199429
git-marcopitra <marcomorete73@hotmail.com>
2024-06-18 01:28:27 +0100

    :sparkles: feat: add onlySPAM and some utils endpoints

--- constants/nft.ts
+++ constants/nft.ts
@@ -9,4 +9,5 @@ export const NFT: ReadonlyArray<string> = [
   'fbb4e6bc-7c71-42a6-80c2-196f876198e0', // Gommies
   '07231735-96de-4710-8e11-52c61a482578', // Prime Machin
   '200220c4-d7fa-41e8-a8be-63d208556336', // Unbound
+  'ae2db678-d698-4a0b-a224-00c66fa9813a', // OnlySPAM
 ];
