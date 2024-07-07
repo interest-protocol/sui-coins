27722d67ae8fa9ff2a6a20beb5eabebc915e4353
git-marcopitra <marcomorete73@hotmail.com>
2024-07-08 00:08:00 +0100

    :bug: fix: headers

--- next.config.js
+++ next.config.js
@@ -4,9 +4,12 @@ const nextConfig = {
       {
         source: '/api/v1/quest/galxe/:path*',
         headers: [
-          { key: 'Access-Control-Request-Method', value: 'GET' },
-          { key: 'Origin', value: 'https://dashboard.galxe.com' },
-          { key: 'Access-Control-Request-Headers', value: 'Content-Type' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET' },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://dashboard.galxe.com',
+          },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
         ],
       },
     ];
