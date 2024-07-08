6755b1e2735c287e633e9c90b98e998b2fece11e
git-marcopitra <marcomorete73@hotmail.com>
2024-07-08 15:46:37 +0100

    :sparkles: feat: add address validation

--- pages/api/v1/quest/galxe/create-pool.ts
+++ pages/api/v1/quest/galxe/create-pool.ts
@@ -1,3 +1,4 @@
+import { isValidSuiAddress } from '@mysten/sui.js/utils';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import { findQuestProfile } from '@/server/lib/quest';
@@ -7,6 +8,9 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     if (req.method === 'GET') {
       const address = req.query.address as string;
+
+      if (!isValidSuiAddress(address))
+        return res.status(400).send(new Error('Invalid address'));
 
       const profile = await findQuestProfile(address);
 
--- pages/api/v1/quest/galxe/faucet.ts
+++ pages/api/v1/quest/galxe/faucet.ts
@@ -1,3 +1,4 @@
+import { isValidSuiAddress } from '@mysten/sui.js/utils';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { values } from 'ramda';
 
@@ -8,6 +9,9 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     if (req.method === 'GET') {
       const address = req.query.address as string;
+
+      if (!isValidSuiAddress(address))
+        return res.status(400).send(new Error('Invalid address'));
 
       const profile = await findQuestProfile(address);
 
