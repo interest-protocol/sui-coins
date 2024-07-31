9a0fd11687893173dae47cf78d01e1049e347495
git-marcopitra <marcomorete73@hotmail.com>
2024-07-31 20:32:24 +0100

    :lipstick: fix: header

--- components/wallet/index.tsx
+++ components/wallet/index.tsx
@@ -10,42 +10,30 @@ const Wallet: FC = () => {
   const currentAccount = useCurrentAccount();
 
   return (
-    <Box
-      display="flex"
-      alignItems="center"
-      justifyContent="flex-end"
-      flexDirection={['row-reverse', 'row-reverse', 'row-reverse', 'row']}
-    >
-      <Box display="flex" gap="m">
-        <a
-          href="https://exultant-lycra-b2b.notion.site/Interest-Protocol-a3ed3682872f4b72ae881bf17b5d0be3"
-          target="_blank"
-          rel="noreferrer"
+    <Box gap="s" display="flex" alignItems="center" justifyContent="flex-end">
+      <a
+        href="https://exultant-lycra-b2b.notion.site/Interest-Protocol-a3ed3682872f4b72ae881bf17b5d0be3"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Button
+          bg="#FFDA34"
+          color="#000000"
+          variant="filled"
+          px={['xs', 'xs', 's']}
+          nHover={{ bg: 'warning' }}
         >
-          <Button
-            bg="#FFDA34"
-            color="#000000"
-            variant="filled"
-            p={['xs', 'xs', 's']}
-            nHover={{ bg: 'warning' }}
-          >
-            Guide
-          </Button>
-        </a>
-        <Box
-          gap="l"
-          justifyContent="flex-end"
-          display={['none', 'none', 'none', 'flex']}
-        >
-          <MovementNetwork />
-        </Box>
-        {!!currentAccount && <Profile />}
+          Guide
+        </Button>
+      </a>
+      <Box
+        gap="l"
+        justifyContent="flex-end"
+        display={['none', 'none', 'none', 'flex']}
+      >
+        <MovementNetwork />
       </Box>
-      {!currentAccount && (
-        <Box display="flex">
-          <ConnectWalletButton />
-        </Box>
-      )}
+      {currentAccount ? <Profile /> : <ConnectWalletButton />}
     </Box>
   );
 };
