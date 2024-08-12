d2204aba3d46f7689425d23bdad3f4cecf7bdd3e
git-marcopitra <marcomorete73@hotmail.com>
2024-08-12 10:25:09 +0100

    :fire: chore: delete the wipe file

--- server/lib/quest/wipe.ts
+++
@@ -1,8 +0,0 @@
-import dbConnect from '@/server';
-import QuestProfileModel from '@/server/model/quest-profile';
-
-export const wipeQuestProfile = async () => {
-  await dbConnect();
-
-  return QuestProfileModel.deleteMany();
-};
