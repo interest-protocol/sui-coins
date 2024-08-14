ecc2eb8434f1f6fe88d828dcd4f6dff89f86891a
git-marcopitra <marcomorete73@hotmail.com>
2024-08-14 17:01:29 +0100

    :construction: wip: work in progress

--- hooks/use-dca-sdk/index.ts
+++ hooks/use-dca-sdk/index.ts
@@ -1,12 +1,11 @@
 import { DcaSDK } from '@interest-protocol/dca-sdk';
-import { getFullnodeUrl } from '@mysten/sui/client';
 
 import { Network } from '@/constants';
 
 import { useNetwork } from '../use-network';
 
-const DCATestnet = new DcaSDK(getFullnodeUrl('testnet'));
-const DCAMainnet = new DcaSDK(getFullnodeUrl('mainnet'));
+const DCATestnet = new DcaSDK({ network: 'testnet' });
+const DCAMainnet = new DcaSDK({ network: 'mainnet' });
 
 const DCA_SDK = {
   [Network.TESTNET]: DCATestnet,
