ea4efb8adf84def90f8967dcec449d8cb2a6b324
Mario Batalha <mariobatalha9@gmail.com>
2024-08-15 17:46:42 +0100

    :recycle: refactor: add new style to footer

--- components/layout/footer/index.tsx
+++ components/layout/footer/index.tsx
@@ -1,112 +1,92 @@
 import { Box, Typography } from '@interest-protocol/ui-kit';
+import Link from 'next/link';
 import { FC } from 'react';
+import { v4 } from 'uuid';
 
-import {
-  DiscordSVG,
-  GithubSVG,
-  HeartSVG,
-  IPXSVG,
-  TelegramSVG,
-  XSVG,
-} from '@/svg';
+import { HeartSVG, IPXSVG } from '@/svg';
 
-const Footer: FC = () => (
-  <Box display="flex" flexDirection="column" textTransform="uppercase">
-    <Box textAlign="center" mb="l">
-      <Typography variant="label" size="medium">
-        The website is maintained &
-      </Typography>
-      <Typography
-        gap="2xs"
-        size="medium"
+import { SOCIAL_LINK } from './social-link.data';
+
+const Footer: FC = () => {
+  const IPXLandingPage = 'https://www.interestprotocol.com/';
+  return (
+    <Box display="flex" flexDirection="column" textTransform="uppercase">
+      <Box
+        gap="0.5rem"
+        width="100%"
         display="flex"
-        variant="label"
+        alignItems="center"
+        flexDirection="column"
         justifyContent="center"
       >
-        Made with
-        <HeartSVG maxHeight="1.125rem" maxWidth="1.125rem" width="100%" />
-        By Interest Protocol
-      </Typography>
-    </Box>
-    <Box textAlign="center" my="2xl">
-      <Typography variant="label" size="medium">
-        Follow us
-      </Typography>
-      <Box display="flex" gap="xs" justifyContent="center" mt="s">
-        <a
-          href="https://discord.com/invite/interestprotocol"
+        <Typography variant="label" size="medium">
+          The website is maintained &
+        </Typography>
+        <Typography
+          gap="2xs"
+          size="medium"
+          display="flex"
+          variant="label"
+          justifyContent="center"
+        >
+          Made with
+          <HeartSVG maxHeight="1.125rem" maxWidth="1.125rem" width="100%" />
+          By Interest Protocol
+        </Typography>
+      </Box>
+      <Box
+        my="2xl"
+        display="flex"
+        alignItems="center"
+        flexDirection="column"
+        justifyContent="center"
+      >
+        <Typography variant="label" size="medium">
+          Follow us
+        </Typography>
+        <Box display="flex" gap="xs" justifyContent="center" mt="s">
+          {SOCIAL_LINK.map(({ title, pathname, Icon }) => {
+            return (
+              <Link
+                key={v4()}
+                href={pathname}
+                target="_blank"
+                rel="noreferrer"
+                title={`Visit our page on ${title}`}
+              >
+                <Box
+                  p="xs"
+                  width="2.5rem"
+                  height="2.5rem"
+                  border="1px solid"
+                  borderRadius="full"
+                  borderColor="outline"
+                >
+                  <Icon maxHeight="100%" maxWidth="100%" width="100%" />
+                </Box>
+              </Link>
+            );
+          })}
+        </Box>
+      </Box>
+      <Box
+        py="m"
+        display="flex"
+        borderTop="1px solid"
+        borderColor="outlineVariant"
+        justifyContent="center"
+      >
+        <Link
           target="_blank"
           rel="noreferrer"
+          href={IPXLandingPage}
+          title="Visit our landing page"
         >
-          <Box
-            p="xs"
-            width="2.5rem"
-            height="2.5rem"
-            border="1px solid"
-            borderRadius="full"
-            borderColor="outline"
-          >
-            <DiscordSVG maxHeight="100%" maxWidth="100%" width="100%" />
-          </Box>
-        </a>
-        <a
-          href="https://github.com/interest-protocol/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Box
-            p="xs"
-            width="2.5rem"
-            height="2.5rem"
-            border="1px solid"
-            borderRadius="full"
-            borderColor="outline"
-          >
-            <GithubSVG maxHeight="100%" maxWidth="100%" width="100%" />
-          </Box>
-        </a>
-        <a
-          href="https://t.me/interestprotocol"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Box
-            p="xs"
-            width="2.5rem"
-            height="2.5rem"
-            border="1px solid"
-            borderRadius="full"
-            borderColor="outline"
-          >
-            <TelegramSVG maxHeight="100%" maxWidth="100%" width="100%" />
-          </Box>
-        </a>
-        <a href="https://x.com/IPXSui" target="_blank" rel="noreferrer">
-          <Box
-            p="xs"
-            width="2.5rem"
-            height="2.5rem"
-            border="1px solid"
-            borderRadius="full"
-            borderColor="outline"
-          >
-            <XSVG maxHeight="100%" maxWidth="100%" height="100%" />
-          </Box>
-        </a>
+          <IPXSVG maxHeight="2.5rem" maxWidth="2.5rem" width="100%" />
+        </Link>
       </Box>
     </Box>
-    <Box
-      py="m"
-      display="flex"
-      borderTop="1px solid"
-      borderColor="#C6C6CA"
-      justifyContent="center"
-    >
-      <a href="https://x.com/IPXSui" target="_blank" rel="noreferrer">
-        <IPXSVG maxHeight="2.5rem" maxWidth="2.5rem" width="100%" />
-      </a>
-    </Box>
-  </Box>
-);
+  );
+};
 
 export default Footer;
