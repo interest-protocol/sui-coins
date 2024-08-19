f1b60183793e3b2341cd557587188eb3523977de
git-marcopitra <marcomorete73@hotmail.com>
2024-08-19 21:13:10 +0100

    :sparkles: feat: avoid  to pay fees

--- views/airdrop/airdrop.constants.ts
+++ views/airdrop/airdrop.constants.ts
@@ -1,3 +1,7 @@
 export const RATE_LIMIT_DELAY = 5000;
 
 export const BATCH_SIZE = 500;
+
+export const EXCLUDED_FEE_COINS = [
+  '0x1fc50c2a9edf1497011c793cb5c88fd5f257fd7009e85a489392f388b1118f82::tusk::TUSK',
+];
