a368cf430d9840df1a6fb21570e7678392d3eb51
Mario Batalha <mariobatalha9@gmail.com>
2024-09-19 00:18:28 +0100

    :construction: feat: work in progress

--- views/airdrop/airdrop-form/airdrop-custom-amount-method.tsx
+++ views/airdrop/airdrop-form/airdrop-custom-amount-method.tsx
@@ -1,30 +1,37 @@
 import { Box, Typography } from '@interest-protocol/ui-kit';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import AirdropCommonAmountTextField from './airdrop-common-amount-text-field';
 import AirdropCustomAmount from './airdrop-custom-amount';
 import AirdropCustomAmountTextArea from './airdrop-custom-amount-text-area';
 
-const AirdropCustomAmountMethod: FC = () => (
-  <Box>
-    <Box pb="m">
-      <Typography variant="body" size="large" mb="m">
-        3. Choose amount & who to send:
-      </Typography>
-      <AirdropCustomAmount
-        isCustomAmountActive={false}
-        handleCustomAmount={() => console.log('handle custom amount')}
-        message="Activate this option to send different amounts simultaneously to various wallets."
-      />
-      <Typography variant="body" size="small">
-        Enter Amount to Send
-      </Typography>
-      <Box>
-        <AirdropCommonAmountTextField />
+const AirdropCustomAmountMethod: FC = () => {
+  const [isActive, setIsActive] = useState<boolean>(false);
+
+  const handleCustomAmount = () => {
+    setIsActive(!isActive);
+  };
+  return (
+    <Box>
+      <Box pb="m">
+        <Typography variant="body" size="large" mb="m">
+          3. Choose amount & who to send:
+        </Typography>
+        <AirdropCustomAmount
+          isCustomAmountActive={isActive}
+          handleCustomAmount={handleCustomAmount}
+          message="Activate this option to send different amounts simultaneously to various wallets."
+        />
+        <Typography variant="body" size="medium">
+          Enter amount to send {isActive && 'for each'}
+        </Typography>
+        <Box>
+          <AirdropCommonAmountTextField />
+        </Box>
       </Box>
+      <AirdropCustomAmountTextArea />
     </Box>
-    <AirdropCustomAmountTextArea />
-  </Box>
-);
+  );
+};
 
 export default AirdropCustomAmountMethod;
