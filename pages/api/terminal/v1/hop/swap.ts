e7c4b6964bc1821815c0738f539f254d30efd295
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2024-09-18 11:14:17 +0100

    ✨ Add terminal endpoints (#409)

---
+++ pages/api/terminal/v1/hop/swap.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import NextCors from 'nextjs-cors';
+import invariant from 'tiny-invariant';
+
+import { swap, SwapArg } from '@/server/lib/hop';
+import { handleServerError } from '@/server/utils';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    await NextCors(req, res, {
+      methods: ['POST'],
+      optionsSuccessStatus: 200,
+      origin: process.env.ORIGIN ?? '*',
+    });
+
+    const data = req.body as SwapArg;
+
+    invariant(data, 'Missing parameters');
+
+    const txb = await swap(data);
+
+    res.status(200).json({ txb });
+  } catch (e) {
+    res.status(500).send(handleServerError(e));
+  }
+}
