70030380e68c13524c2c5f4fe94b36283f58e1c0
git-marcopitra <marcomorete73@hotmail.com>
2024-09-19 03:42:24 +0100

    :sparkles: feat: add clear button

--- views/merge/merge-button.tsx
+++ views/merge/merge-button.tsx
@@ -4,7 +4,7 @@ import { useFormContext, useWatch } from 'react-hook-form';
 import toast from 'react-hot-toast';
 
 import { useWeb3 } from '@/hooks/use-web3';
-import { MergeSVG } from '@/svg';
+import { MergeSVG, TrashSVG } from '@/svg';
 
 import { useMergeCoins } from './merge.hooks';
 import { IMergeForm } from './merge.types';
@@ -13,13 +13,21 @@ const MergeButton: FC = () => {
   const mergeCoins = useMergeCoins();
   const { coins, mutate } = useWeb3();
   const [loading, setLoading] = useState(false);
-  const { control, reset } = useFormContext<IMergeForm>();
+  const { control, reset, setValue } = useFormContext<IMergeForm>();
+
   const ignored = useWatch({ control, name: 'ignored' });
 
   const allCoinsToMerge = coins.filter(({ objects }) => objects.length > 1);
+
   const coinsToMerge = allCoinsToMerge.filter(
     ({ type }) => !ignored.includes(type)
   );
+
+  const fillIgnored = () =>
+    setValue(
+      'ignored',
+      allCoinsToMerge.map(({ type }) => type)
+    );
 
   const handleMergeCoins = async () => {
     if (loading) return;
@@ -46,8 +54,21 @@ const MergeButton: FC = () => {
       display="grid"
       maxWidth="25rem"
       borderRadius="xs"
-      gridTemplateColumns="1fr 2fr"
+      gridTemplateColumns="1fr 2fr 3fr"
     >
+      <Button
+        p="s"
+        gap="m"
+        bg="error"
+        variant="filled"
+        onClick={fillIgnored}
+        justifyContent="center"
+        color="onSurfaceContainer"
+        disabled={loading || !(allCoinsToMerge.length - ignored.length)}
+        nHover={{ bg: 'errorContainer', color: 'onErrorContainer' }}
+      >
+        <TrashSVG maxWidth="1rem" maxHeight="1rem" width="100%" />
+      </Button>
       <Button
         p="s"
         gap="m"
