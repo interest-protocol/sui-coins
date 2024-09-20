b295ec9773e80def13a4941a4363250eda48d307
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2024-09-20 12:06:30 +0100

    ⬆️ Upgrade DCA (#418)

--- hooks/use-dca-sdk/index.ts
+++ hooks/use-dca-sdk/index.ts
@@ -1,24 +1,7 @@
 import { DcaSDK } from '@interest-protocol/dca-sdk';
 
-import { Network } from '@/constants';
+const dcaSdk = new DcaSDK();
 
-import { useNetwork } from '../use-network';
-
-const DCATestnet = new DcaSDK({
-  network: 'testnet',
-});
-
-const DCAMainnet = new DcaSDK();
-
-const DCA_SDK = {
-  [Network.TESTNET]: DCATestnet,
-  [Network.MAINNET]: DCAMainnet,
-};
-
-const useDcaSdk = () => {
-  const network = useNetwork();
-
-  return DCA_SDK[network];
-};
+const useDcaSdk = () => dcaSdk;
 
 export default useDcaSdk;
