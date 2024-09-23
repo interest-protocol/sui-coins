0a390aa3bd28a70cce7cee380bd3354313ff54d2
Atila Aeseo Sousa <Atila.Aeseo.Sousa@bancobai.ao>
2024-09-23 08:48:39 +0100

    :bug: fix: destroyed dca message fixed

--- views/dca-orders/dca-orders.data.ts
+++ views/dca-orders/dca-orders.data.ts
@@ -1,5 +1,5 @@
 export enum DCAOrdersMessagesEnum {
   deletingDca = 'We are deleting the DCA, and you will let you know when it is done',
-  deletingFailure = 'Failure in deleting the dca, please try again or contact the support team',
-  deletingSuccess = 'Your DCA was successfully deleted, and you can check it on the Explorer',
+  deletingFailure = 'Failure in destroying the dca, please try again or contact the support team',
+  deletingSuccess = 'Your DCA was successfully destroyed, and you can check it on the Explorer',
 }
