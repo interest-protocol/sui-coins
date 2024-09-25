facff77e2dc766a74cfc0e58c9678cc3f8dce4fc
git-marcopitra <marcomorete73@hotmail.com>
2024-09-25 13:52:31 +0100

    :sparkles: feat: add download button to addresses

---
+++ utils/nft/index.ts
@@ -0,0 +1,21 @@
+export const getHoldersJSON = async (
+  id: string,
+  name: string,
+  date: number
+) => {
+  const nfts = await fetch(`/api/auth/v1/nft-collection?id=${id}`, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  }).then((res) => res.json());
+
+  const jsonFile =
+    'data:text/json;charset=utf-8,' +
+    encodeURIComponent(JSON.stringify(nfts.holders));
+
+  const linkElement = document.createElement('a');
+
+  linkElement.setAttribute('href', jsonFile);
+  linkElement.setAttribute('download', `${name}-holders-${date}.json`);
+  linkElement.click();
+  linkElement.remove();
+};
