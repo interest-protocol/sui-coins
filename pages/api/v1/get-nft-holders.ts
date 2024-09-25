1641af9109627343e15ca7caf76430950b73f228
git-marcopitra <marcomorete73@hotmail.com>
2024-09-25 13:57:35 +0100

    :sparkles: feat: add endpoint to get holders

--- pages/api/v1/get-nft-holders.ts
+++ pages/api/v1/get-nft-holders.ts
@@ -3,15 +3,21 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import dbConnect from '@/server';
 import NFTCollectionModel from '@/server/model/nft-collection';
 
-const handler = async (_: NextApiRequest, res: NextApiResponse) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     await dbConnect();
 
-    const docs = await NFTCollectionModel.find();
+    const id = req.query.id;
 
-    if (!docs.length) return res.status(204).json([]);
+    if (id) {
+      const doc = await NFTCollectionModel.findOne({ collectionId: id });
 
-    return res.status(200).json(docs);
+      if (!doc)
+        return res.status(204).json({ data: { message: 'Nothing found!' } });
+
+      return res.status(200).json(doc);
+    }
+    return res.status(401).json({ data: { message: 'Missing Params!' } });
   } catch (e) {
     res.status(500).send(e);
   }
