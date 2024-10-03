96025f7c2c4fbf243bb7e673ce28a191f96a496b
git-marcopitra <marcomorete73@hotmail.com>
2024-10-03 18:58:39 +0100

    :bug: fix: low dca fee

--- constants/fees.ts
+++ constants/fees.ts
@@ -7,6 +7,6 @@ export const EXCHANGE_FEE_PERCENTAGE = EXCHANGE_FEE * 100; // percentage
 export const EXCHANGE_FEE_BPS = EXCHANGE_FEE_PERCENTAGE * 100; // bps
 
 // 0.5%
-export const DCA_FEE = 0.005; // Real numeric value
+export const DCA_FEE = 0.003; // Real numeric value
 export const DCA_FEE_PERCENTAGE = DCA_FEE * 100; // percentage
 export const DCA_FEE_BPS = DCA_FEE_PERCENTAGE * 100; // bps
