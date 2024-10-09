6cce56f72c11d4694f2819457fcd0500b7198e57
Antonio Kipanda Cardoso <34079870+KipandaJr@users.noreply.github.com>
2024-10-09 21:10:15 +0100

    :sparkles: New Swap Short URL (#422)

--- views/swap/swap-init-manager.tsx
+++ views/swap/swap-init-manager.tsx
@@ -5,6 +5,7 @@ import { useFormContext } from 'react-hook-form';
 import { useReadLocalStorage } from 'usehooks-ts';
 
 import { LOCAL_STORAGE_VERSION, Network } from '@/constants';
+import { STRICT_TOKENS, STRICT_TOKENS_MAP } from '@/constants/coins';
 import { getAllCoinsPrice } from '@/hooks/use-get-multiple-token-price-by-type/use-get-multiple-token-price-by-type.utils';
 import { useNetwork } from '@/hooks/use-network';
 import { useWeb3 } from '@/hooks/use-web3';
@@ -88,7 +89,63 @@ const SwapInitManager: FC = () => {
       .then((data) => form.setValue(`${field}.usdPrice`, data[token.type]))
       .catch(console.log);
 
-    return token.type;
+    return STRICT_TOKENS_MAP[network][token.type]?.symbol || token.type;
+  };
+
+  const setTokenType = async (
+    from: string | undefined,
+    to: string | undefined
+  ) => {
+    const TokenUSDC = STRICT_TOKENS[network].find(
+      (token) => token.symbol == 'USDC'
+    );
+
+    if (!from && !to)
+      return await Promise.all([
+        setDefaultToken(SUI_TYPE_ARG as `0x${string}`, 'from'),
+        setDefaultToken(TokenUSDC?.type as `0x${string}`, 'to'),
+      ]);
+
+    if (to && !from) {
+      if (!STRICT_TOKENS_MAP[network][to]) {
+        if (TokenUSDC?.symbol == to) from = SUI_TYPE_ARG;
+        if (STRICT_TOKENS_MAP[network][SUI_TYPE_ARG]?.symbol === to)
+          from = TokenUSDC?.type;
+      }
+    }
+
+    if (from && !to) {
+      if (!STRICT_TOKENS_MAP[network][from]) {
+        if (TokenUSDC?.symbol == from) to = SUI_TYPE_ARG;
+        if (STRICT_TOKENS_MAP[network][SUI_TYPE_ARG]?.symbol === from)
+          to = TokenUSDC?.type;
+      }
+    }
+
+    return await Promise.all([
+      from
+        ? setDefaultToken(
+            STRICT_TOKENS_MAP[network][from]?.type ||
+              STRICT_TOKENS[network].find(
+                (token) => token.symbol == from || token.type == from
+              )?.type ||
+              from,
+            'from'
+          )
+        : undefined,
+      to
+        ? from !== to
+          ? setDefaultToken(
+              STRICT_TOKENS_MAP[network][to]?.type ||
+                STRICT_TOKENS[network].find(
+                  (token) => token.symbol == to || token.type == to
+                )?.type ||
+                to,
+              'to'
+            )
+          : undefined
+        : undefined,
+    ]);
   };
 
   useEffect(() => {
@@ -103,10 +160,10 @@ const SwapInitManager: FC = () => {
     (async () => {
       const searchParams = new URLSearchParams(asPath.split('?')[1]);
 
-      const [fromType, toType] = await Promise.all([
-        setDefaultToken(from as `0x${string}`, 'from'),
-        from !== to ? setDefaultToken(to as `0x${string}`, 'to') : undefined,
-      ]);
+      const [fromType, toType] = await setTokenType(
+        from as `0x${string}`,
+        to as `0x${string}`
+      );
 
       searchParams.delete('from');
       searchParams.delete('to');
@@ -116,11 +173,13 @@ const SwapInitManager: FC = () => {
 
       form.setValue('loading', false);
 
-      const params = searchParams.toString();
-
-      updateURL(`${pathname}${params ? `?${params}` : ''}`);
+      updateURL(
+        `${pathname}?from=${searchParams.get('from')}&to=${searchParams.get(
+          'to'
+        )}`
+      );
     })();
-  }, []);
+  }, [network, to, from]);
 
   return null;
 };
