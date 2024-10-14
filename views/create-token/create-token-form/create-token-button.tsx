2825f4eebfc912ddaaa0069224b797e63164126b
git-marcopitra <marcomorete73@hotmail.com>
2024-10-14 16:38:22 +0100

    :recycle: chore: reduce de create token fee

--- views/create-token/create-token-form/create-token-button.tsx
+++ views/create-token/create-token-form/create-token-button.tsx
@@ -114,7 +114,7 @@ const CreateTokenButton: FC<CreateTokenButtonProps> = ({
     : !coinsMap[SUI_TYPE_ARG]
       ? 'No SUI found! Add Sui to your wallet'
       : coinsMap[SUI_TYPE_ARG].balance.lt(CREATE_TOKEN_SUI_FEE)
-        ? 'It costs 25 Sui to create a coin'
+        ? 'It costs 10 Sui to create a coin'
         : null;
 
   const disabled = loading || !!error;
