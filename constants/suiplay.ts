016b640918649087d28a21fdbd61176739ca35b7
git-marcopitra <marcomorete73@hotmail.com>
2024-10-21 21:22:06 +0100

    :sparkles: feat: add sort suiplay nfts by importance

--- constants/suiplay.ts
+++ constants/suiplay.ts
@@ -2,14 +2,14 @@ import { SUIPLAY_HOLDERS } from './nft';
 
 export const SUIPLAY_SOULBOUND = [
   {
-    tier: 'The Exalted',
-    image: '/images/suiplay/exalted.png',
-    holders: SUIPLAY_HOLDERS['The Exalted'].length,
-  },
-  {
     tier: 'The Mythics',
     image: '/images/suiplay/mythics.png',
     holders: SUIPLAY_HOLDERS['The Mythics'].length,
+  },
+  {
+    tier: 'The Exalted',
+    image: '/images/suiplay/exalted.png',
+    holders: SUIPLAY_HOLDERS['The Exalted'].length,
   },
   {
     tier: 'All',
