d42e8265705cb9948b49838790161a4b52a89060
Atila <atilaesousa@gmail.com>
2024-11-27 21:11:56 +0100

    :bug: fix: reset form on change method

--- views/airdrop/index.tsx
+++ views/airdrop/index.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@interest-protocol/ui-kit';
-import { FC, useState } from 'react';
-import { useFormContext } from 'react-hook-form';
+import { FC, useEffect, useState } from 'react';
+import { useFormContext, useWatch } from 'react-hook-form';
 
 import Layout from '@/components/layout';
 
@@ -9,8 +9,14 @@ import AirdropProgressIndicator from './airdrop-progress-indicator';
 import AirdropUploadStatus from './airdrop-upload-status';
 
 const Airdrop: FC = () => {
-  const { reset } = useFormContext();
+  const { reset, control } = useFormContext();
   const [isProgressView, setIsProgressView] = useState(false);
+
+  const method = useWatch({ control, name: 'method' });
+
+  useEffect(() => {
+    reset({ method });
+  }, [method]);
 
   return (
     <Layout title="Airdrop" features={['coins', 'nfts']}>
