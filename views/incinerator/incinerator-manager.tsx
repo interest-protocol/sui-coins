28e65349a49a2535aad50a24ec6c42f820c9acb7
git-marcopitra <marcomorete73@hotmail.com>
2024-11-28 12:54:12 +0100

    :bug: fix: incinerator main page

--- views/incinerator/incinerator-manager.tsx
+++ views/incinerator/incinerator-manager.tsx
@@ -13,7 +13,6 @@ const IncineratorManager: FC = () => {
   const currentAccount = useCurrentAccount();
   const { control, setValue } = useFormContext<IncineratorForm>();
   const {
-    delay,
     error,
     mutate,
     objects,
