626ae1792101a28311a62ac6215cbcf1033f83b9
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2024-12-13 04:46:45 +0100

    🎮  Sui Play (#534)

--- views/merge/merge-field.tsx
+++ views/merge/merge-field.tsx
@@ -48,26 +48,34 @@ const MergeField: FC<MergeFieldProps> = ({
         <Button
           p="2xs"
           width="100%"
+          overflow="hidden"
           variant="outline"
           borderRadius="xs"
           onClick={openModal}
           nHover={{ color: 'unset' }}
           borderColor="outlineVariant"
           PrefixIcon={
-            <TokenIcon withBg type={type} symbol={symbol} network={network} />
+            <Box display="flex" gap="s" alignItems="center">
+              <TokenIcon withBg type={type} symbol={symbol} network={network} />
+            </Box>
           }
           SuffixIcon={
             <ChevronDownSVG width="100%" maxWidth="1rem" maxHeight="1rem" />
           }
         >
-          <Typography
-            size="large"
-            width="100%"
-            variant="body"
-            color="onSurface"
-          >
-            {symbol}
-          </Typography>
+          <Box width="100%" display="flex" justifyContent="flex-start">
+            <Typography
+              size="large"
+              variant="body"
+              maxWidth="12ch"
+              color="onSurface"
+              overflow="hidden"
+              whiteSpace="nowrap"
+              textOverflow="ellipsis"
+            >
+              {symbol}
+            </Typography>
+          </Box>
         </Button>
         <Button
           isIcon
@@ -79,14 +87,33 @@ const MergeField: FC<MergeFieldProps> = ({
           <MinusSVG maxWidth="1.5rem" maxHeight="1.5rem" width="100%" />
         </Button>
       </Box>
-      <Box display="flex" justifyContent="space-between" px="2xs">
+      <Box
+        display="flex"
+        overflow="hidden"
+        justifyContent="space-between"
+        px="2xs"
+      >
         <Box display="flex" color="outline" gap="0.5rem" alignItems="center">
           <Box width="1rem" height="1rem">
             <SubtractBoxSVG width="100%" maxWidth="100%" maxHeight="100%" />
           </Box>
-          <Typography size="small" variant="body" fontSize="s">
-            {FixedPointMath.toNumber(balance, decimals)} {symbol}
+          <Typography size="small" fontSize="s" variant="body">
+            {FixedPointMath.toNumber(balance, decimals)}
           </Typography>
+          {symbol && (
+            <Typography
+              ml="-0.2rem"
+              size="small"
+              fontSize="s"
+              variant="body"
+              maxWidth="12ch"
+              overflow="hidden"
+              whiteSpace="nowrap"
+              textOverflow="ellipsis"
+            >
+              {symbol}
+            </Typography>
+          )}
         </Box>
         <Typography variant="body" size="small" color="outline">
           {objectsCount} objects to merge
