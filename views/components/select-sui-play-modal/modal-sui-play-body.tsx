00754103dcfcbf6d203730026d6da9634415b6df
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2024-12-24 01:12:04 +0100

    :sparkles: add remote sui play holders (#545)

--- views/components/select-sui-play-modal/modal-sui-play-body.tsx
+++ views/components/select-sui-play-modal/modal-sui-play-body.tsx
@@ -3,36 +3,43 @@ import { FC } from 'react';
 import { v4 } from 'uuid';
 
 import { SUIPLAY_SOULBOUND } from '@/constants';
-import { SUIPLAY_LAST_UPDATE_AT } from '@/constants/nft';
+import { useSuiPlayHolders } from '@/hooks/use-sui-play-holders';
 
 import { SelectSuiPlayModalBodyProps } from './select-sui-play-modal.types';
 import SuiPlayModalItem from './sui-play-modal-item';
 
 const ModalSuiPlayBody: FC<SelectSuiPlayModalBodyProps> = ({
   handleSelectTier,
-}) => (
-  <Box
-    m="s"
-    gap="s"
-    flex="1"
-    display="flex"
-    overflowY="auto"
-    bg="lowContainer"
-    flexDirection="column"
-  >
-    {SUIPLAY_SOULBOUND?.map((info) => (
-      <SuiPlayModalItem
-        key={v4()}
-        selected={false}
-        updatedAt={SUIPLAY_LAST_UPDATE_AT}
-        onClick={() =>
-          handleSelectTier(info.tier as 'The Exalted' | 'The Mythics' | 'All')
-        }
-        {...info}
-        tier={info.tier as 'The Exalted' | 'The Mythics' | 'All'}
-      />
-    ))}
-  </Box>
-);
+}) => {
+  const { data } = useSuiPlayHolders();
+
+  if (!data) return null;
+
+  return (
+    <Box
+      m="s"
+      gap="s"
+      flex="1"
+      display="flex"
+      overflowY="auto"
+      bg="lowContainer"
+      flexDirection="column"
+    >
+      {SUIPLAY_SOULBOUND.map((info) => (
+        <SuiPlayModalItem
+          key={v4()}
+          selected={false}
+          updatedAt={data.lastUpdateAt}
+          holders={data.holders[info.tier]}
+          onClick={() =>
+            handleSelectTier(info.tier as 'The Exalted' | 'The Mythics' | 'All')
+          }
+          {...info}
+          tier={info.tier as 'The Exalted' | 'The Mythics' | 'All'}
+        />
+      ))}
+    </Box>
+  );
+};
 
 export default ModalSuiPlayBody;
