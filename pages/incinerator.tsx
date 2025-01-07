832d17148f3ca0ccd1b8dd34f57b8c2d0773b85c
git-marcopitra <marcomorete73@hotmail.com>
2025-01-07 13:58:15 +0100

    :fire: chore: remove old code

--- pages/incinerator.tsx
+++ pages/incinerator.tsx
@@ -1,12 +1,8 @@
-import { useCurrentWallet } from '@mysten/dapp-kit';
 import { NextPage } from 'next';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 
 import { SEO } from '@/components';
 import Layout from '@/components/layout';
-import { INCINERATOR_EXTERNAL_LINK } from '@/constants';
 import Incinerator from '@/views/incinerator';
 import {
   IncineratorForm,
@@ -15,9 +11,6 @@ import {
 import IncineratorInitManager from '@/views/incinerator/incinerator-init-manager';
 
 const IncineratorPage: NextPage = () => {
-  const { currentWallet } = useCurrentWallet();
-  const { push } = useRouter();
-
   const form = useForm<IncineratorForm>({
     defaultValues: {
       search: '',
@@ -28,12 +21,6 @@ const IncineratorPage: NextPage = () => {
       tab: IncineratorTabEnum.All,
     },
   });
-  useEffect(() => {
-    if (currentWallet?.name === 'Sui Wallet') {
-      console.log('>> Redirecting to Sui Wallet safe website');
-      push(INCINERATOR_EXTERNAL_LINK);
-    }
-  }, [currentWallet]);
 
   return (
     <FormProvider {...form}>
