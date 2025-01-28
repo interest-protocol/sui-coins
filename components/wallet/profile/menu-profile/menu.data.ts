48099e3f967cc19355674657f2b12d9146726e05
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2025-01-28 02:58:19 +0100

    :fire: chore: remove comments

--- components/wallet/profile/menu-profile/menu.data.ts
+++ components/wallet/profile/menu-profile/menu.data.ts
@@ -3,27 +3,6 @@ import { AssetsSVG, GlobeSVG, LogoutSVG, SwitchSVG } from '@/components/svg';
 import { ProfileMenuItemProps } from '../profile.types';
 
 export const MENU_PROFILE_DATA: ReadonlyArray<ProfileMenuItemProps> = [
-  /*{
-    name: 'activity',
-    description: 'activity',
-    Icon: ActivitySVG,
-    hasBorder: false,
-    disabled: true,
-  },
-  {
-    name: 'assets',
-    description: 'assets',
-    Icon: AssetsSVG,
-    hasBorder: false,
-    disabled: true,
-  },
-  {
-    name: 'viewInExplorer',
-    description: 'View in explorer',
-    Icon: ExternalSVG,
-    hasBorder: false,
-    disabled: false,
-  },*/
   {
     name: 'changeExplorer',
     description: 'Change explorer',
