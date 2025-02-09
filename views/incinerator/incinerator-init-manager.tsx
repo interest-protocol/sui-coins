1db99ebee137426e64af679442c132cf9f8b5e48
Atila <atilaesousa@gmail.com>
2025-02-09 19:55:37 +0100

    :bug: fix: using local storage to save the state

--- views/incinerator/incinerator-init-manager.tsx
+++ views/incinerator/incinerator-init-manager.tsx
@@ -1,25 +1,40 @@
 import { useCurrentWallet } from '@mysten/dapp-kit';
 import { useRouter } from 'next/router';
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect } from 'react';
+import { useLocalStorage } from 'usehooks-ts';
 
-import { INCINERATOR_EXTERNAL_LINK, RoutesEnum } from '@/constants';
+import {
+  EXPLORER_STORAGE_KEY,
+  INCINERATOR_EXTERNAL_LINK,
+  LOCAL_STORAGE_VERSION,
+  Routes,
+  RoutesEnum,
+} from '@/constants';
 import { useWeb3 } from '@/hooks/use-web3';
 
 const IncineratorInitManager: FC = () => {
   const { loading } = useWeb3();
   const { replace, pathname } = useRouter();
   const { currentWallet } = useCurrentWallet();
-  const [hasRedirected, setHasRedirected] = useState(false);
+  const [hasRedirected, setHasRedirected] = useLocalStorage<boolean>(
+    `${LOCAL_STORAGE_VERSION}-${EXPLORER_STORAGE_KEY}`,
+    false
+  );
 
   useEffect(() => {
-    if (currentWallet?.name === 'Sui Wallet' && !hasRedirected) {
+    if (currentWallet?.name === 'Sui Wallet' && !loading && !hasRedirected) {
       console.log('>> Redirecting to Sui Wallet safe website');
       setHasRedirected(true);
       replace(INCINERATOR_EXTERNAL_LINK);
-    } else if (hasRedirected && pathname === '/incinerator') {
-      replace(RoutesEnum.Swap);
     }
-  }, [currentWallet, loading, hasRedirected, pathname, replace]);
+  }, [currentWallet, loading, hasRedirected, replace, setHasRedirected]);
+
+  useEffect(() => {
+    if (hasRedirected && pathname === '/incinerator') {
+      replace(Routes[RoutesEnum.Swap]);
+      setHasRedirected(false);
+    }
+  }, [hasRedirected, pathname, replace, setHasRedirected]);
 
   return null;
 };
