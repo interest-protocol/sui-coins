d5841a17b148e15b6a309b2a027c0dbcc3b0ca57
Atila <atilaesousa@gmail.com>
2025-02-09 12:50:35 +0100

    :bug: fix: incinerator redirect

--- views/incinerator/incinerator-init-manager.tsx
+++ views/incinerator/incinerator-init-manager.tsx
@@ -1,21 +1,25 @@
 import { useCurrentWallet } from '@mysten/dapp-kit';
 import { useRouter } from 'next/router';
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 import { INCINERATOR_EXTERNAL_LINK } from '@/constants';
 import { useWeb3 } from '@/hooks/use-web3';
 
 const IncineratorInitManager: FC = () => {
   const { loading } = useWeb3();
-  const { replace } = useRouter();
+  const { replace, pathname } = useRouter();
   const { currentWallet } = useCurrentWallet();
+  const [hasRedirected, setHasRedirected] = useState(false);
 
   useEffect(() => {
-    if (currentWallet?.name === 'Sui Wallet') {
+    if (currentWallet?.name === 'Sui Wallet' && !hasRedirected) {
       console.log('>> Redirecting to Sui Wallet safe website');
+      setHasRedirected(true);
       replace(INCINERATOR_EXTERNAL_LINK);
+    } else if (hasRedirected && pathname === '/incinerator') {
+      replace('/');
     }
-  }, [currentWallet, loading]);
+  }, [currentWallet, loading, hasRedirected, pathname, replace]);
 
   return null;
 };
